66a4117c33d1525f5c4c13266c2cc43297af4899
Saeed Rasooli <saeed.gnu@gmail.com>
2012-01-23 15:18:12 +0330

    cleaning rc.py

--- src/rc.py
+++ src/rc.py
@@ -1,56 +1,58 @@
-save=0
-newline='\n'
-auto_update=True
-auto_set_for=True
-auto_set_out=True
-sort=True
-lower=False
+save = 0
+newline = '\n'
+auto_update = True
+auto_set_for = True
+auto_set_out = True
+sort = True
+lower = False
 utf8_check = True
 
-remove_tags=False
+remove_tags = False
 ## TagName : RemoveBetween
-tags={
-'i'	:0,
-'I'	:0,
-'b'	:0,
-'B'	:0,
-'a'	:0,
-'A'	:0,
-'tr'	:0,
-'font'	:0,
-'FONT'	:0,
-'TR'	:0,
-'tr'	:0,
-'td'	:0,
-'TD'	:0,
-'HR'	:0,
-'hr'	:0,
-'TABLE'	:0,
-'table'	:0,
-'P'	:0,
-'p'	:0,
-'U'	:0,
-'u'	:0,
-'sup'   :0,
-'SUP'   :0}
+tags = {
+    'font': 0,
+    'FONT': 0,
+    'table': 0,
+    'TABLE': 0,
+    'sup'   :0,
+    'SUP'   :0,
+    'tr': 0,
+    'TR': 0,
+    'td': 0,
+    'TD': 0,
+    'hr': 0,
+    'HR': 0,
+    'i': 0,
+    'I': 0,
+    'b': 0,
+    'B': 0,
+    'a': 0,
+    'A': 0,
+    'p': 0,
+    'P': 0,
+    'u': 0,
+    'U': 0,
+    'k': 0,
+    'K': 0,
+}
 
-wrap_out=True
-wrap_err=True
-wrap_edit=True
-wrap_dbe=True
-color_bg_out=(9898, 0, 10945)
-color_bg_err=(2443, 0, 0)
-color_bg_edit=(65535, 65535, 65535)
-color_bg_dbe=(65535, 65535, 65535)
-color_font_out=(65535, 65535, 65535)
-color_font_err=(65535, 0, 0)
-color_font_edit=(0, 0, 0)
-color_font_dbe=(0, 0, 0)
+wrap_out = True
+wrap_err = True
+wrap_edit = True
+wrap_dbe = True
+color_bg_out = (9898, 0, 10945)
+color_bg_err = (2443, 0, 0)
+color_bg_edit = (65535, 65535, 65535)
+color_bg_dbe = (65535, 65535, 65535)
+color_font_out = (65535, 65535, 65535)
+color_font_err = (65535, 0, 0)
+color_font_edit = (0, 0, 0)
+color_font_dbe = (0, 0, 0)
 
 ## Reverse Options:
-matchWord=True
-showRel='None'
-autoSaveStep=1000
-minRel=0.3
-maxNum=-1
-includeDefs=True
+matchWord = True
+showRel = 'None'
+autoSaveStep = 1000
+minRel = 0.3
+maxNum = -1
+includeDefs = True
