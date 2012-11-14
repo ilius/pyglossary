e75028d6df9a1cb7f4b63ca313c4667d9a022fec
kubtek <kubtek@gmail.com>
2012-11-14 15:21:15 +0400

    annotate Glossary.data

--- src/glossary.py
+++ src/glossary.py
@@ -109,6 +109,20 @@ class Glossary:
     writeDesc = []
     descFormat = {}
     descExt = {}
+    """
+    a list if tuples: ('key', 'definition') or ('key', 'definition', dict() )
+    in general we should assume the tuple may be of arbitrary length >= 2
+    known dictionary keys:
+        data[i][2]['alts'] - list of alternates, filled by bgl reader
+        data[i][2]['defis'] - list of alternative definitions. 
+            For example, run (eng.) may be 1. verb, 2. noun, 3. adjective.
+            self.data[i][1] contains the main definition of the word, the verb, in the example.
+            While additional definitions goes to self.data[i][2]['defis'] list, noun and adjective,
+            in the example.
+            You may merge additional definition with the main definition if the target dictionary 
+            format does not support several definitions per word.
+    """
+    data = []
     # load plugins
     if plugDir:
         for f in os.listdir(plugDir):
@@ -154,10 +168,6 @@ class Glossary:
     def __init__(self, info=[], data=[], resPath=''):
         self.info = []
         self.setInfos(info, True)
-        # a list if tuples: ('key', 'definition') or ('key', 'definition', dict() )
-        # in general we should assume the tuple may be of arbitrary length >= 2
-        # known dictionary keys:
-        #     data[i][2]['alts'] - list of alternates, filled by bgl reader
         self.data = data
         self.filename = ''
         self.resPath = resPath
