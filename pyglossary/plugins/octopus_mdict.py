534f87ec19a5d8f3d280071cb93df2363541df21
Saeed Rasooli <saeed.gnu@gmail.com>
2013-07-02 19:13:56 +0430

    fix import bug in octopus_mdict

--- pyglossary/plugins/octopus_mdict.py
+++ pyglossary/plugins/octopus_mdict.py
@@ -16,7 +16,7 @@
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 ## GNU General Public License for more details.
 
-from readmdict import MDX, MDD
+from pyglossary.readmdict import MDX, MDD
 import os
 
 enable = True
