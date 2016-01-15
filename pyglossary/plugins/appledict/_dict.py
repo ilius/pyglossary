355508d894406b2701f33cc1b95048441852e544
ivan tkachenko <ratijas@users.noreply.github.com>
2016-01-15 19:45:24 +0800

    appledict splitted to many little functions

--- pyglossary/plugins/appledict/_dict.py
+++ pyglossary/plugins/appledict/_dict.py
@@ -35,20 +35,10 @@ def truncate(text, length=449):
         text = text[:pos]
     return text
 
-def write_xml(glos, filename, cleanHTML, frontBackMatter):
-    try:
-        from bs4 import BeautifulSoup
-    except:
-        try:
-            from BeautifulSoup import BeautifulSoup
-        except:
-            cleanHTML = False
+def dictionary_begin(glos, f, frontBackMatter):
     # progress bar
-    ui = glos.ui
-    if ui:
-        ui.progressStart()
-
-    f = open(filename, 'wb')
+    if glos.ui:
+        glos.ui.progressStart()
 
     # write header
     f.write('<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -58,63 +48,121 @@ def write_xml(glos, filename, cleanHTML, frontBackMatter):
         with open(frontBackMatter, 'r') as front_back_matter:
             f.write(front_back_matter.read())
 
-    # write entries
+def get_beautiful_soup():
+    try:
+        from bs4 import BeautifulSoup
+    except:
+        try:
+            from BeautifulSoup import BeautifulSoup
+        except:
+            return None
+    return BeautifulSoup
+
+def id_generator():
+    # closure
     entry_ids = set([])
-    total = len(glos.data)
-    close_tag = re.compile('<(BR|HR)>', re.IGNORECASE)
-    nonprintable = re.compile('[\x00-\x07\x0e-\x1f]')
-    img_tag = re.compile('<IMG (.*?)>', re.IGNORECASE)
-    for index, item in enumerate(glos.data):
-        # strip double quotes and html tags
-        title = re.sub('<[^<]+?>|"|[<>]|\xef\xbb\xbf', '', item[0])
-        if not title:
-            continue
+
+    def generate_id(title):
         # use MD5 hash of title string as id
-        id = hashlib.md5(title).hexdigest()
+        id = '_' + hashlib.md5(title).hexdigest()
 
         # check entry id duplicates
         while id in entry_ids:
-            id = id + '_'
+            id += '_'
         entry_ids.add(id)
+        return id
+
+    return generate_id
+
+def indexes_generator():
+    # it will be factory that atcs according to glossary language
+    def generate_indexes(title, alts):
+        s = '    <d:index d:value="%s"/>\n'%truncate(title)
+        #   alternative items as index also
+        for alt in alts:
+            if alt != title:
+                s += '    <d:index d:value="%s"/>\n'%truncate(alt)
+        return s
+    return generate_indexes
+
+
+close_tag = re.compile('<(BR|HR)>', re.IGNORECASE)
+nonprintable = re.compile('[\x00-\x07\x0e-\x1f]')
+img_tag = re.compile('<IMG (.*?)>', re.IGNORECASE)
+
+def format_clean_content(title, body, BeautifulSoup):
+    # nice header to display
+    content = ('<h1>%s</h1>\n'%title) + body
+    # xhtml is strict
+    if BeautifulSoup:
+        soup  = BeautifulSoup(content, from_encoding='utf8')
+        content = str(soup)
+    else:
+        content = close_tag.sub('<\g<1> />', content)
+        content = img_tag.sub('<img \g<1>/>', content)
+    content = content.replace('&nbsp;', '&#160;')
+    content = nonprintable.sub('', content)
+    return content
+
+
+title_re = re.compile('<[^<]+?>|"|[<>]|\xef\xbb\xbf')
+
+def normilize_title(title):
+    """strip double quotes and html tags."""
+    return title_re.sub('', title)
+
+
+def write_entries(glos, f, cleanHTML):
+    if cleanHTML:
+        BeautifulSoup = get_beautiful_soup()
+    else:
+        BeautifulSoup = None
+
+    # write entries
+    generate_id = id_generator()
+    generate_indexes = indexes_generator()
+    total = float(len(glos.data))
+
+    for i, item in enumerate(glos.data):
+        title = normilize_title(item[0])
+        if not title:
+            continue
+
+        id = generate_id(title)
+
+        begin_entry = '<d:entry id="%(id)s" d:title="%(title)s">\n' % {
+            'id': id,
+            'title': truncate(title.replace('&', '&amp;'), 1126)
+        }
+        f.write(begin_entry)
+
         # get alternatives list
         try:
             alts = item[2]['alts']
         except:
             alts = []
 
-        # begin entry
-        f.write('<d:entry id="%(id)s" d:title="%(title)s">\n'%{'id':id, 'title':truncate(title.replace('&', '&amp;'), 1126)})
+        indexes = generate_indexes(title, alts)
+        f.write(indexes)
 
-        # index values
-        #   title as index
-        f.write('    <d:index d:value="%s"/>\n'%truncate(title))
-        #   alternative items as index also
-        for alt in alts:
-            if alt != title:
-                f.write('    <d:index d:value="%s"/>\n'%truncate(alt))
-
-        # nice header to display
-        content = ('<h1>%s</h1>\n'%title) + item[1]
-        # xhtml is strict
-        if cleanHTML:
-            soup  = BeautifulSoup(content, from_encoding='utf8')
-            content = str(soup)
-        else:
-            content = close_tag.sub('<\g<1> />', content)
-            content = img_tag.sub('<img \g<1>/>', content)
-        content = content.replace('&nbsp;', '&#160;')
-        content = nonprintable.sub('', content)
+        content = format_clean_content(title, item[1], BeautifulSoup)
         f.write(content)
 
-        # end entry
-        f.write('\n</d:entry>\n')
+        end_entry = '\n</d:entry>\n'
+        f.write(end_entry)
 
-        if index%1000==0 and ui:
-            ui.progress(1.0*index/total)
-    # end dictionary
+        if i % 1000 == 0 and glos.ui:
+            glos.ui.progress(i / total)
+
+def dictionary_end(glos, f):
     f.write('</d:dictionary>\n')
-    f.close()
 
     # end progress bar
-    if ui:
-        ui.progressEnd()
+    if glos.ui:
+        glos.ui.progressEnd()
+
+def write_xml(glos, filename, cleanHTML, frontBackMatter):
+    with open(filename, 'wb') as f:
+        dictionary_begin(glos, f, frontBackMatter)
+        write_entries(glos, f, cleanHTML)
+        dictionary_end(glos, f)
