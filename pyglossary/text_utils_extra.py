0e081a5907de560107719deecf43400e3205dd00
Saeed Rasooli <saeed.gnu@gmail.com>
2016-02-04 00:36:50 +0330

    move commented functions from text_utils.py to text_utils_extra.py

---
+++ pyglossary/text_utils_extra.py
@@ -0,0 +1,105 @@
+## Python 3.x:
+def intToBinStr(n, stLen=0):
+    bs = b''
+    while n>0:
+        bs = bytes([n & 255]) + bs
+        n >>= 8
+    return bs.rjust(stLen, b'\x00')
+
+## Python 3.x:
+intToBinStr = lambda n, stLen=0: bytes((
+    (n >> (i<<3)) & 0xff for i in range(int(ceil(log(n, 256)))-1, -1, -1)
+)).rjust(stLen, b'\x00')
+
+
+## Python 3.x:
+def binStrToInt(bs):
+    n = 0
+    for c in bs:
+        n = (n << 8) + c
+    return n
+
+
+def textProgress(n=100, t=0.1):
+    import time
+    for i in xrange(n):
+        sys.stdout.write('\b\b\b\b##%3d'%(i+1));time.sleep(t)
+    sys.stdout.write('\b\b\b')
+
+def locate(lst, val):
+    n = len(lst)
+    if n==0:
+        return
+    if val < lst[0]:
+        return -0.5
+    if val == lst[0]:
+        return 0
+    if val == lst[-1]:
+        return n-1
+    if val > lst[-1]:
+        return n-0.5
+    si = 0 # start index
+    ei = n # end index
+    while ei-si>1:
+        mi = (ei+si)/2 # middle index
+        if lst[mi] == val :
+            return mi
+        elif lst[mi] > val :
+            ei=mi
+            continue
+        else:
+            si=mi
+            continue
+    if ei-si==1:
+        return si+0.5
+
+def locate2(lst, val, ind=1):
+    n = len(lst)
+    if n==0:
+        return
+    if val<lst[0][ind]:
+        return -0.5
+    if val==lst[0][ind]:
+        return 0
+    if val==lst[-1][ind]:
+        return n-1
+    if val>lst[-1][ind]:
+        return n-0.5
+    si = 0
+    ei = n
+    while ei-si>1:
+        mi = (ei+si)/2
+        if lst[mi][ind]==val:
+            return mi
+        elif lst[mi][ind]>val:
+            ei = mi
+            continue
+        else:
+            si = mi
+        continue
+    if ei-si==1:
+        return si+0.5
+
+def xml2dict(xmlText):
+    from xml.etree.ElementTree import XML, tostring
+    xmlElems = XML(xmlText)
+    for elem in xmlElems:
+        elemText = tostring(elem)
+        try:
+            elem[0]
+            elemElems = xml2dict()
+        except:
+            pass
+
+def sortby(lst, n, reverse=False):
+    nlist = [(x[n], x) for x in lst]
+    nlist.sort(None, None, reverse)
+    return [val for (key, val) in nlist]
+
+
+def sortby_inplace(lst, n, reverse=False):
+    lst[:] = [(x[n], x) for x in lst]
+    lst.sort(None, None, reverse)
+    lst[:] = [val for (key, val) in lst]
+    return
+
