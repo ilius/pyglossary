1f0b67538f981a55f77b513b1d3c56aa4287d13a
Saeed Rasooli <saeed.gnu@gmail.com>
2016-02-04 11:29:36 +0330

    copy gregorian.py from https://github.com/ilius/starcal, speed boost and cleanings

--- pyglossary/gregorian.py
+++ pyglossary/gregorian.py
@@ -1,31 +1,33 @@
 # -*- coding: utf-8 -*-
-##
-## Copyright (C) 2009-2010 Saeed Rasooli <saeed.gnu@gmail.com> (ilius)
-## Copyright (C) 2007 Mehdi Bayazee <Bayazee@Gmail.com>
-##
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License,    or
-## (at your option) any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License along
-## with this program. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+#
+# Copyright (C) Saeed Rasooli <saeed.gnu@gmail.com>
+# Copyright (C) 2007 Mehdi Bayazee <Bayazee@Gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/gpl.txt>.
+# Also avalable in /usr/share/common-licenses/GPL on Debian systems
+# or /usr/share/licenses/common/GPL3/license.txt on ArchLinux
 
 
-## Gregorian calendar:
-## http://en.wikipedia.org/wiki/Gregorian_calendar
+##    Gregorian calendar:
+##        http://en.wikipedia.org/wiki/Gregorian_calendar
 
 name = 'gregorian'
 desc = 'Gregorian'
 origLang = 'en'
 
-monthName = ('January', 'February', 'March', 'April', 'May', 'June',
-             'July', 'August', 'September', 'October', 'November', 'December')
+monthName = ('January','February','March','April','May','June',
+             'July','August','September','October','November','December')
 
 monthNameAb = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
@@ -38,18 +40,24 @@ getMonthsInYear = lambda y: 12
 epoch = 1721426
 minMonthLen = 29
 maxMonthLen = 31
+avgYearLen = 365.2425 ## FIXME
 
 options = ()
+
+from datetime import datetime
 
 def save():
     pass
 
 def isLeap(y):
-    if y < 1:
+    if y<1:
         y += 1
     return y%4==0 and not ( y%100==0 and y%400!=0 )
 
 def to_jd(year, month, day):
+    if year > 0:## > 1.5x faster
+        return datetime(year, month, day).toordinal() + 1721425
+
     # Python 2.x and 3.x:
     if month <= 2:
         tm = 0
@@ -64,11 +72,15 @@ def to_jd(year, month, day):
 
 
 def jd_to(jd) :
-    assert isinstance(jd, int)
+    ordinal = int(jd) - 1721425
+    if ordinal > 0:## > 4x faster
+        dt = datetime.fromordinal(ordinal)
+        return (dt.year, dt.month, dt.day)
+
     ##wjd = floor(jd - 0.5) + 0.5
-    (qc, dqc) = divmod(jd - epoch, 146097) ## qc ~~ quadricent
-    (cent, dcent) = divmod(dqc, 36524)
-    (quad, dquad) = divmod(dcent, 1461)
+    qc, dqc = divmod(jd - epoch, 146097) ## qc ~~ quadricent
+    cent, dcent = divmod(dqc, 36524)
+    quad, dquad = divmod(dcent, 1461)
     yindex = dquad//365 ## divmod(dquad, 365)[0]
     year = qc*400 + cent*100 + quad*4 + yindex + (cent!=4 and yindex!=4)
     yearday = jd - to_jd(year, 1, 1)
@@ -83,11 +95,14 @@ def jd_to(jd) :
     #leapadj = 0 if jd < to_jd(year, 3, 1) else (1 if isLeap(year) else 2)
     month = ((yearday+leapadj) * 12 + 373) // 367
     day = jd - to_jd(year, month, 1) + 1
-    return (year, month, day)
+    return int(year), int(month), int(day)
 
 def getMonthLen(y, m):
     if m==12:
         return to_jd(y+1, 1, 1) - to_jd(y, 12, 1)
     else:
         return to_jd(y, m+1, 1) - to_jd(y, m, 1)
+
+J0001 = to_jd(1, 1, 1)
+J1970 = to_jd(1970, 1, 1)
 
