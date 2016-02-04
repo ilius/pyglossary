5ab2f6ac7341626ebbe1c4158f9ff8a643c9ec42
Saeed Rasooli <saeed.gnu@gmail.com>
2016-02-04 12:53:52 +0330

    add plugin Lingoes Source (LDF)

---
+++ pyglossary/plugins/lingoes_ldf.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+import logging
+log = logging.getLogger('root')
+
+from formats_common import *
+
+enable = True
+format = 'LingoesLDF'
+description = 'Lingoes Source (LDF)'
+extentions = ['.ldf']
+readOptions = []
+writeOptions = []
+
+infoKeys = [
+    'title',
+    'description',
+    'author',
+    'email',
+    'website',
+    'copyright',
+]
+
+def read(glos, filename):
+    fileObj = FileLineWrapper(open(filename))
+    lineStack = []
+    def addDataEntry(lineStack):
+        if not lineStack:
+            return
+        if len(lineStack) < 2:
+            log.error('invalid block near line %s in file %s'%(fileObj.line, filename))
+            return
+        word = lineStack[0]
+        defi = '\n'.join(lineStack[1:])
+        defi = defi.replace('<br/>', '\n') ## FIXME
+        glos.data.append((
+            word,
+            defi,
+            #{'alts': []},
+        ))
+
+    for line in fileObj:
+        line = line.strip()
+        if not line.startswith('###'):
+            if line:
+                lineStack.append(line)
+            break
+        parts = line[3:].split(':')
+        if not parts:
+            continue
+        key = parts[0].lower()
+        value = ' '.join(parts[1:]).strip()
+        glos.setInfo(key, value)
+    ## info lines finished
+
+    for line in fileObj:
+        line = line.strip()
+        if line:
+            lineStack.append(line)
+        else:
+            addDataEntry(lineStack)
+            lineStack = []
+
+    addDataEntry(lineStack)
+
+
+
+def write(glos, filename):
+    g = glos
+    newline = '\n'
+    head = newline.join([
+        '###%s: %s'%(
+            key.capitalize(),
+            g.getInfo(key),
+        )
+        for key in infoKeys
+    ])
+    head += '\n'
+    g.writeTxt(
+        (newline, newline*2),
+        filename=filename,
+        writeInfo=False,
+        rplList=(
+            ('\n', '<br/>'),
+        ),
+        ext='.ldf',
+        head=head,
+    )
+
+
+
+
+
+
