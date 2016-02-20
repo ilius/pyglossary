0ca536efa1b5f002dc29b56e7d164bb0c5e8ff42
ivan tkachenko <ratijas@users.noreply.github.com>
2016-02-20 19:20:14 +0800

    dsl: new `flawless_dsl` parser

---
+++ pyglossary/plugins/dsl/flawless_dsl/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# flawless_dsl/__init__.py
+#
+""" only clean flawless DSL markup on output!"""
+#
+# Copyright (C) 2016 Ratijas <ratijas.t@me.com>
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# You can get a copy of GNU General Public License along this program
+# But you can always get it from http://www.gnu.org/licenses/gpl.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+from . import layer
+from . import tag
+from .main import (
+    FlawlessDSLParser,
+    parse,
+)
---
+++ pyglossary/plugins/dsl/flawless_dsl/tag.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+# flawless_dsl/tag.py
+#
+""" internal stuff.  Tag class"""
+#
+# Copyright (C) 2016 Ratijas <ratijas.t@me.com>
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# You can get a copy of GNU General Public License along this program
+# But you can always get it from http://www.gnu.org/licenses/gpl.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
+
+from collections import namedtuple
+
+
+Tag = namedtuple('Tag', ['opening', 'closing'])
+
+def __repr__(self):
+    if self.opening == self.closing:
+        return 'Tag(%r)' % self.opening
+    return 'Tag(%s, %s)' % self
+
+Tag.__repr__ = __repr__
+del __repr__
+
+def was_opened(stack, tag):
+    """
+    check if tag was opened at some layer before.
+
+    :param stack: Iterable[layer.Layer]
+    :param tag: tag.Tag
+    :return: bool
+    """
+    if not len(stack):
+        return False
+    layer = stack[-1]
+    if tag in layer:
+        return True
+    return was_opened(stack[:-1], tag)
+
+
+predefined = ['m', '*', 'ex', 'i', 'c']
+
+def canonical_order(tags):
+    """
+    arrange tags in canonical way, where (outermost to innermost):
+    m  >  *  >  ex  >  i  >  c
+    with all other tags follow them in alphabetical order.
+
+    :param tags: Iterable[Tag]
+    :return: List
+    """
+    result = []
+    tags = list(tags)
+    for predef in predefined:
+        t = next((t for t in tags if t.closing == predef), None)
+        if t:
+            result.append(t)
+            tags.remove(t)
+    result.extend(sorted(tags, key=lambda x: x.opening))
+    return result
+
+
+def index_of_layer_containing_tag(stack, tag):
+    """
+    return zero based index of layer with `tag` or None
+
+    :param stack: Iterable[layer.Layer]
+    :param tag: str
+    :return: int | None
+    """
+    for i, layer in enumerate(reversed(stack)):
+        for t in layer.tags:
+            if t.closing == tag:
+                return len(stack) - i - 1
+    return None
