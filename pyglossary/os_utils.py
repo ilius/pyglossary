8303bbf1d6d7667399a038206afa5f05799aeb96
Saeed Rasooli <saeed.gnu@gmail.com>
2016-03-20 20:41:57 +0330

    move `chdir` class from plugins/formats_common.py into os_utils.py

---
+++ pyglossary/os_utils.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+
+class chdir:
+    """
+    mkdir + chdir shortcut to use with `with` statement.
+
+        >>> print(os.getcwd())  # -> "~/projects"
+        >>> with chdir('my_directory', create=True):
+        >>>     print(os.getcwd())  # -> "~/projects/my_directory"
+        >>>     # do some work inside new 'my_directory'...
+        >>> print(os.getcwd())  # -> "~/projects"
+        >>> # automatically return to previous directory.
+    """
+    def __init__(self, directory, create=False, clear=False):
+        self.oldpwd = None
+        self.dir = directory
+        self.create = create
+        self.clear = clear
+
+    def __enter__(self):
+        self.oldpwd = os.getcwd()
+        if os.path.exists(self.dir):
+            if self.clear:
+                shutil.rmtree(self.dir)
+                os.makedirs(self.dir)
+        elif self.create:
+            os.makedirs(self.dir)
+        os.chdir(self.dir)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.oldpwd)
+        self.oldpwd = None
--- pyglossary/plugins/formats_common.py
+++ pyglossary/plugins/formats_common.py
@@ -21,36 +21,6 @@ log = logging.getLogger('root')
 from pyglossary import core
 from pyglossary.file_utils import FileLineWrapper
 from pyglossary.text_utils import toStr, toUnicode
+from pyglossary.os_utils import chdir
 
 
-class chdir:
-    """
-    mkdir + chdir shortcut to use with `with` statement.
-
-        >>> print(os.getcwd())  # -> "~/projects"
-        >>> with chdir('my_directory', create=True):
-        >>>     print(os.getcwd())  # -> "~/projects/my_directory"
-        >>>     # do some work inside new 'my_directory'...
-        >>> print(os.getcwd())  # -> "~/projects"
-        >>> # automatically return to previous directory.
-    """
-    def __init__(self, directory, create=False, clear=False):
-        self.oldpwd = None
-        self.dir = directory
-        self.create = create
-        self.clear = clear
-
-    def __enter__(self):
-        import shutil
-        self.oldpwd = os.getcwd()
-        if os.path.exists(self.dir):
-            if self.clear:
-                shutil.rmtree(self.dir)
-                os.makedirs(self.dir)
-        elif self.create:
-            os.makedirs(self.dir)
-        os.chdir(self.dir)
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        os.chdir(self.oldpwd)
-        self.oldpwd = None
