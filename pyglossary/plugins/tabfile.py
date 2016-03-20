a8e1cbbf4f2ffc85fc323553713bb905b7ac4c75
Saeed Rasooli <saeed.gnu@gmail.com>
2016-03-20 09:19:00 +0330

    move some codes from `tabfile.Reader` into a new base class `TextGlossaryReader`

--- pyglossary/plugins/tabfile.py
+++ pyglossary/plugins/tabfile.py
@@ -11,69 +11,17 @@ writeOptions = [
     'writeInfo',
 ]
 
-from pyglossary.file_utils import fileCountLines
+from pyglossary.text_reader import TextGlossaryReader
 
-class Reader(object):
-    def __init__(self, glos, hasInfo=True):
-        self._glos = glos
-        self._filename = ''
-        self._fp = None
-        self._hasInfo = True
-        self._leadingLinesCount = 0
-        self._pendingEntries = []
-        self._len = None
-    def open(self, filename):
-        self._filename = filename
-        self._fp = open(filename)
-        if self._hasInfo:
-            self._loadInfo()
-    def close(self):
-        if not self._fp:
-            return
-        try:
-            self._fp.close()
-        except:
-            log.exception('error while closing tabfile')
-        self._fp = None
-    def __len__(self):
-        if self._len is None:
-            log.warn('Try not to use len(reader) as it takes extra time')
-            self._len = fileCountLines(self._filename) - self._leadingLinesCount
-        return self._len
-    __iter__ = lambda self: self
-    def next(self):
-        try:
-            return self._pendingEntries.pop(0)
-        except IndexError:
-            pass
-        ###
-        wordDefi = self._nextWordDefi()
-        if not wordDefi:
-            return
-        word, defi = wordDefi
-        ###
-        return Entry(word, defi)
 
-    def _loadInfo(self):
-        self._pendingEntries = []
-        self._leadingLinesCount = 0
-        try:
-            while True:
-                wordDefi = self._nextWordDefi()
-                if not wordDefi:
-                    continue
-                word, defi = wordDefi
-                if not word.startswith('#'):
-                    self._pendingEntries.append(Entry(word, defi))
-                    break
-                self._leadingLinesCount += 1
-                word = word.lstrip('#')
-                if not word:
-                    continue
-                self._glos.setInfo(word, defi)
-        except StopIteration:
-            pass
-    def _nextWordDefi(self):
+class Reader(TextGlossaryReader):
+    def isInfoWord(self, word):
+        return word.startswith('#')
+
+    def fixInfoWord(self, word):
+        return word.lstrip('#')
+
+    def nextPair(self):
         if not self._fp:
             raise StopIteration
         line = self._fp.readline()
---
+++ pyglossary/text_reader.py
@@ -0,0 +1,76 @@
+from pyglossary.file_utils import fileCountLines
+from pyglossary.entry import Entry
+
+class TextGlossaryReader(object):
+    def __init__(self, glos, hasInfo=True):
+        self._glos = glos
+        self._filename = ''
+        self._fp = None
+        self._hasInfo = True
+        self._leadingLinesCount = 0
+        self._pendingEntries = []
+        self._len = None
+    def open(self, filename):
+        self._filename = filename
+        self._fp = open(filename)
+        if self._hasInfo:
+            self.loadInfo()
+    def close(self):
+        if not self._fp:
+            return
+        try:
+            self._fp.close()
+        except:
+            log.exception('error while closing tabfile')
+        self._fp = None
+    def loadInfo(self):
+        self._pendingEntries = []
+        self._leadingLinesCount = 0
+        try:
+            while True:
+                wordDefi = self.nextPair()
+                if not wordDefi:
+                    continue
+                word, defi = wordDefi
+                if not self.isInfoWord(word):
+                    self._pendingEntries.append(Entry(word, defi))
+                    break
+                self._leadingLinesCount += 1
+                word = self.fixInfoWord(word)
+                if not word:
+                    continue
+                self._glos.setInfo(word, defi)
+        except StopIteration:
+            pass
+    def next(self):
+        try:
+            return self._pendingEntries.pop(0)
+        except IndexError:
+            pass
+        ###
+        wordDefi = self.nextPair()
+        if not wordDefi:
+            return
+        word, defi = wordDefi
+        ###
+        return Entry(word, defi)
+    def __len__(self):
+        if self._len is None:
+            log.warn('Try not to use len(reader) as it takes extra time')
+            self._len = fileCountLines(self._filename) - self._leadingLinesCount
+        return self._len
+    __iter__ = lambda self: self
+
+    def isInfoWord(self, word):
+        raise NotImplementedError
+
+    def fixInfoWord(self, word):
+        raise NotImplementedError
+
+    def nextPair(self):
+        raise NotImplementedError
+
+
+
+
+
