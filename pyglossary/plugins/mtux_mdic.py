13e00a2e05a444ef7d76c968dc8afabd34412cc0
Saeed Rasooli <saeed.gnu@gmail.com>
2016-03-21 21:54:45 +0430

    mtux_mdic.py: migrate to Reader class

--- pyglossary/plugins/mtux_mdic.py
+++ pyglossary/plugins/mtux_mdic.py
@@ -9,8 +9,10 @@ extentions = ['.m2', '.sdb']
 readOptions = []
 writeOptions = []
 
+from sqlite3 import connect
+
 infoKeys = [
-    'dbname',
+    'dbname',## name OR dbname? FIXME
     'author',
     'version',
     'direction',
@@ -21,37 +23,71 @@ infoKeys = [
     'description',
 ]
 
-def read(glos, filename):
-    from sqlite3 import connect
+class Reader(object):
+    def __init__(self, glos):
+        self._glos = glos
+        self._clear()
 
-    ## ???????? name OR dbname ????????????????????
-    con = connect(filename)
-    cur = con.cursor()
-    for key in infoKeys:
-        try:
-            cur.execute('select %s from dbinfo'%key)
-        except:
-            pass
-        else:
-            value = cur.fetchone()[0].encode('utf8')
-            if value!='':
-                glos.setInfo(key, value)
-    cur.execute('select * from word')
-    for x in cur.fetchall():
-        try:
-            w = x[1].encode('utf8')
-            m = x[2].encode('utf8')
-        except:
-            log.error('error while encoding word %s'%x[0])
-        else:
-            glos.addEntry(w, m)
-    cur.close()
-    con.close()
-    return True
+    def _clear(self):
+        self._filename = ''
+        self._con = None
+        self._cur = None
+        
+    def open(self, filename):
+        self._filename = filename
+        self._con = connect(filename)
+        self._cur = self._con.cursor()
+        self.loadInfo()
 
-def read_2(glos, filename):
-    import pyglossary.alchemy as alchemy
-    return alchemy.readSqlite(glos, filename)
+    def close(self):
+        self._cur.close()
+        self._con.close()
+        self._clear()
+
+    def loadInfo(self):
+        for key in infoKeys:
+            try:
+                self._cur.execute('select %s from dbinfo'%key)
+            except:
+                continue
+            value = self._cur.fetchone()[0].encode('utf8')
+            if not value:
+                continue
+            self._glos.setInfo(key, value)
+
+    def __len__(self):
+        self._cur.execute('select count(*) from word')
+        return self._cur.fetchone()[0]
+
+    def getLastId(self):
+        self._cur.execute('select max(id) from word')
+        return self._cur.fetchone()[0]
+
+    def __iter__(self):
+        limit = 500
+        '''
+        limit   time    memory
+        100     1.77    25164
+        500     1.75    25136
+        1000    1.76    25140
+        2000    1.78    25724
+        '''
+        lastId = self.getLastId()
+        for startId in range(0, lastId+1, limit):
+            self._cur.execute('select * from word where id between %s and %s order by id'%(
+                startId,
+                startId + limit - 1,
+            ))
+            for row in self._cur.fetchall():
+                try:
+                    word = row[1].encode('utf8')
+                    defi = row[2].encode('utf8')
+                except:
+                    log.exception('error while encoding row id=%s'%row[0])
+                else:
+                    yield Entry(word, defi)
+
+
 
 
 def write_2(glos, filename):
@@ -64,7 +100,6 @@ def write_3(glos, filename):
     return True
 
 def write(glos, filename):
-    from sqlite3 import connect
     if os.path.exists(filename):
         os.remove(filename)
     con = connect(filename)
