54e02dccbcd1af528a2c7ac130589c0f8060923b
Saeed Rasooli <saeed.gnu@gmail.com>
2016-04-27 01:57:47 +0430

    add scripts/dump.py

---
+++ scripts/dump.py
@@ -0,0 +1,13 @@
+import sys
+from pprint import pformat
+
+from pyglossary.glossary import Glossary
+
+glos = Glossary()
+glos.read(sys.argv[1])
+for entry in glos:
+    print('Words: ' + pformat(entry.getWords()))
+    print('Definitions: ' + pformat(entry.getDefis()))
+    print('-------------------------')
+
+
