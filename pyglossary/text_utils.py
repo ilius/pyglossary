730a8a9ba6c035fcd37481cabbe764003c00a789
Saeed Rasooli <saeed.gnu@gmail.com>
2016-05-04 05:58:04 +0430

    cleaning: text_utils.py: move unused chBaseIntToStr into text_utils_extra.py

--- pyglossary/text_utils.py
+++ pyglossary/text_utils.py
@@ -344,25 +344,6 @@ def binStrToInt(bs):
 
 ###############################################
 
-def chBaseIntToStr(number, base):
-    """
-        reverse function of int(str, base) and long(str, base)
-    """
-    if not 2 <= base <= 36:
-        raise ValueError('base must be in 2..36')
-    abc = string.digits + string.ascii_letters
-    result = ''
-    if number < 0:
-        number = -number
-        sign = '-'
-    else:
-        sign = ''
-    while True:
-        number, rdigit = divmod(number, base)
-        result = abc[rdigit] + result
-        if number == 0:
-            return sign + result
-
 def chBaseIntToList(number, base):
     result = []
     if number < 0:
--- pyglossary/text_utils_extra.py
+++ pyglossary/text_utils_extra.py
@@ -103,3 +103,24 @@ def sortby_inplace(lst, n, reverse=False):
     lst[:] = [val for (key, val) in lst]
     return
 
+
+def chBaseIntToStr(number, base):
+    """
+        reverse function of int(str, base) and long(str, base)
+    """
+    if not 2 <= base <= 36:
+        raise ValueError('base must be in 2..36')
+    abc = string.digits + string.ascii_letters
+    result = ''
+    if number < 0:
+        number = -number
+        sign = '-'
+    else:
+        sign = ''
+    while True:
+        number, rdigit = divmod(number, base)
+        result = abc[rdigit] + result
+        if number == 0:
+            return sign + result
+
+
