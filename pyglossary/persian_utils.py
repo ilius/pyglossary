679e1dd59fad71c048f6767b964ca808f206cd36
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-11 23:06:41 +0430

    pep-8 fixes in persian_utils.py

--- pyglossary/persian_utils.py
+++ pyglossary/persian_utils.py
@@ -1,13 +1,11 @@
 from .text_utils import replacePostSpaceChar
 
+
 def faEditStr(st):
     return replacePostSpaceChar(
-        st.replace('ي', 'ی')\
-           .replace('ك', 'ک')\
-           .replace('ۂ', 'هٔ')\
-           .replace('ہ', 'ه'),
-        '،')
-
-
-
-
+        st.replace('ي', 'ی')
+          .replace('ك', 'ک')
+          .replace('ۂ', 'هٔ')
+          .replace('ہ', 'ه'),
+        '،',
+    )
