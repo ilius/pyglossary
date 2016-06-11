6013988ae28809f7aa72f63fcb96359ec6d9306a
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-11 23:08:02 +0430

    pep-8 fixes in text_utils_extra.py

--- pyglossary/text_utils_extra.py
+++ pyglossary/text_utils_extra.py
@@ -1,18 +1,18 @@
-## Python 3.x:
 def intToBinStr(n, stLen=0):
     bs = b''
-    while n>0:
+    while n > 0:
         bs = bytes([n & 255]) + bs
         n >>= 8
     return bs.rjust(stLen, b'\x00')
 
-## Python 3.x:
-intToBinStr = lambda n, stLen=0: bytes((
-    (n >> (i<<3)) & 0xff for i in range(int(ceil(log(n, 256)))-1, -1, -1)
-)).rjust(stLen, b'\x00')
+
+def intToBinStr(n, stLen=0):
+    return bytes((
+        (n >> (i << 3)) & 0xff
+        for i in range(int(ceil(log(n, 256)))-1, -1, -1)
+    )).rjust(stLen, b'\x00')
 
 
-## Python 3.x:
 def binStrToInt(bs):
     n = 0
     for c in bs:
@@ -23,12 +23,14 @@ def binStrToInt(bs):
 def textProgress(n=100, t=0.1):
     import time
     for i in range(n):
-        sys.stdout.write('\b\b\b\b##%3d'%(i+1));time.sleep(t)
+        sys.stdout.write('\b\b\b\b##%3d' % (i+1))
+        time.sleep(t)
     sys.stdout.write('\b\b\b')
+
 
 def locate(lst, val):
     n = len(lst)
-    if n==0:
+    if n == 0:
         return
     if val < lst[0]:
         return -0.5
@@ -38,47 +40,49 @@ def locate(lst, val):
         return n-1
     if val > lst[-1]:
         return n-0.5
-    si = 0 # start index
-    ei = n # end index
-    while ei-si>1:
-        mi = (ei+si)/2 # middle index
-        if lst[mi] == val :
+    si = 0  # start index
+    ei = n  # end index
+    while ei-si > 1:
+        mi = (ei+si)/2  # middle index
+        if lst[mi] == val:
             return mi
-        elif lst[mi] > val :
-            ei=mi
+        elif lst[mi] > val:
+            ei = mi
             continue
         else:
-            si=mi
+            si = mi
             continue
-    if ei-si==1:
+    if ei-si == 1:
         return si+0.5
+
 
 def locate2(lst, val, ind=1):
     n = len(lst)
-    if n==0:
+    if n == 0:
         return
-    if val<lst[0][ind]:
+    if val < lst[0][ind]:
         return -0.5
-    if val==lst[0][ind]:
+    if val == lst[0][ind]:
         return 0
-    if val==lst[-1][ind]:
+    if val == lst[-1][ind]:
         return n-1
-    if val>lst[-1][ind]:
+    if val > lst[-1][ind]:
         return n-0.5
     si = 0
     ei = n
-    while ei-si>1:
+    while ei-si > 1:
         mi = (ei+si)/2
-        if lst[mi][ind]==val:
+        if lst[mi][ind] == val:
             return mi
-        elif lst[mi][ind]>val:
+        elif lst[mi][ind] > val:
             ei = mi
             continue
         else:
             si = mi
         continue
-    if ei-si==1:
+    if ei-si == 1:
         return si+0.5
+
 
 def xml2dict(xmlText):
     from xml.etree.ElementTree import XML, tostring
@@ -90,6 +94,7 @@ def xml2dict(xmlText):
             elemElems = xml2dict()
         except:
             pass
+
 
 def sortby(lst, n, reverse=False):
     nlist = [(x[n], x) for x in lst]
@@ -122,5 +127,3 @@ def chBaseIntToStr(number, base):
         result = abc[rdigit] + result
         if number == 0:
             return sign + result
-
-
