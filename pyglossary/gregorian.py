4385a08729a40329a3add8a58af7c73bb9d7dd14
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-11 23:09:11 +0430

    pep-8 fixes in gregorian.py

--- pyglossary/gregorian.py
+++ pyglossary/gregorian.py
@@ -19,8 +19,10 @@
 # or /usr/share/licenses/common/GPL3/license.txt on ArchLinux
 
 
-##    Gregorian calendar:
-##        http://en.wikipedia.org/wiki/Gregorian_calendar
+# Gregorian calendar:
+#   http://en.wikipedia.org/wiki/Gregorian_calendar
+
+from datetime import datetime
 
 name = 'gregorian'
 desc = 'Gregorian'
@@ -32,30 +34,39 @@ monthName = ('January', 'February', 'March', 'April', 'May', 'June',
 monthNameAb = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
 
-getMonthName = lambda m, y=None: monthName.__getitem__(m-1)
-getMonthNameAb = lambda m, y=None: monthNameAb.__getitem__(m-1)
 
-getMonthsInYear = lambda y: 12
+def getMonthName(m, y=None):
+    return monthName.__getitem__(m - 1)
+
+
+def getMonthNameAb(m, y=None):
+    return monthNameAb.__getitem__(m - 1)
+
+
+def getMonthsInYear(y):
+    return 12
+
 
 epoch = 1721426
 minMonthLen = 29
 maxMonthLen = 31
-avgYearLen = 365.2425 ## FIXME
+avgYearLen = 365.2425  # FIXME
 
 options = ()
 
-from datetime import datetime
 
 def save():
     pass
 
+
 def isLeap(y):
-    if y<1:
+    if y < 1:
         y += 1
-    return y%4==0 and not ( y%100==0 and y%400!=0 )
+    return y % 4 == 0 and not (y % 100 == 0 and y % 400 != 0)
+
 
 def to_jd(year, month, day):
-    if year > 0:## > 1.5x faster
+    if year > 0:  # > 1.5x faster
         return datetime(year, month, day).toordinal() + 1721425
 
     # Python 2.x and 3.x:
@@ -66,23 +77,23 @@ def to_jd(year, month, day):
     else:
         tm = -2
     # Python >= 2.5:
-    #tm = 0 if month <= 2 else (-1 if isLeap(year) else -2)
+    # tm = 0 if month <= 2 else (-1 if isLeap(year) else -2)
     return epoch - 1 + 365*(year-1) + (year-1)//4 - (year-1)//100 + \
-                 (year-1)//400 + (367*month-362)//12 + tm + day
+        (year-1)//400 + (367*month-362)//12 + tm + day
 
 
-def jd_to(jd) :
+def jd_to(jd):
     ordinal = int(jd) - 1721425
-    if ordinal > 0:## > 4x faster
+    if ordinal > 0:  # > 4x faster
         dt = datetime.fromordinal(ordinal)
         return (dt.year, dt.month, dt.day)
 
-    ##wjd = floor(jd - 0.5) + 0.5
-    qc, dqc = divmod(jd - epoch, 146097) ## qc ~~ quadricent
+    # wjd = floor(jd - 0.5) + 0.5
+    qc, dqc = divmod(jd - epoch, 146097)  # qc ~~ quadricent
     cent, dcent = divmod(dqc, 36524)
     quad, dquad = divmod(dcent, 1461)
-    yindex = dquad//365 ## divmod(dquad, 365)[0]
-    year = qc*400 + cent*100 + quad*4 + yindex + (cent!=4 and yindex!=4)
+    yindex = dquad//365  # divmod(dquad, 365)[0]
+    year = qc*400 + cent*100 + quad*4 + yindex + (cent != 4 and yindex != 4)
     yearday = jd - to_jd(year, 1, 1)
     # Python 2.x and 3.x:
     if jd < to_jd(year, 3, 1):
@@ -92,17 +103,17 @@ def jd_to(jd) :
     else:
         leapadj = 2
     # Python >= 2.5:
-    #leapadj = 0 if jd < to_jd(year, 3, 1) else (1 if isLeap(year) else 2)
+    # leapadj = 0 if jd < to_jd(year, 3, 1) else (1 if isLeap(year) else 2)
     month = ((yearday+leapadj) * 12 + 373) // 367
     day = jd - to_jd(year, month, 1) + 1
     return int(year), int(month), int(day)
 
+
 def getMonthLen(y, m):
-    if m==12:
+    if m == 12:
         return to_jd(y+1, 1, 1) - to_jd(y, 12, 1)
     else:
         return to_jd(y, m+1, 1) - to_jd(y, m, 1)
 
 J0001 = to_jd(1, 1, 1)
 J1970 = to_jd(1970, 1, 1)
-
