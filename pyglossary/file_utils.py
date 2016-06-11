bc0f2382235b73883a1ef62352d5af8519dcff4d
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-11 23:01:40 +0430

    pep-8 fixes in file_utils.py

--- pyglossary/file_utils.py
+++ pyglossary/file_utils.py
@@ -3,11 +3,14 @@ from itertools import (
     repeat,
 )
 
-toBytes = lambda s: bytes(s, 'utf8') if isinstance(s, str) else bytes(s)
+
+def toBytes(s):
+    return bytes(s, 'utf8') if isinstance(s, str) else bytes(s)
+
 
 def fileCountLines(filename, newline='\n'):
-    newline = toBytes(newline)## required? FIXME
-    f = open(filename, 'rb')## or 'r'
+    newline = toBytes(newline)  # required? FIXME
+    f = open(filename, 'rb')  # or 'r'
     bufgen = takewhile(
         lambda x: x, (f.read(1024*1024) for _ in repeat(None))
     )
@@ -15,14 +18,18 @@ def fileCountLines(filename, newline='\n'):
         buf.count(newline) for buf in bufgen if buf
     )
 
+
 class FileLineWrapper(object):
     def __init__(self, f):
         self.f = f
         self.line = 0
+
     def close(self):
         return self.f.close()
+
     def readline(self):
         self.line += 1
         return self.f.readline()
-    __iter__ = lambda self: iter(self.f)
 
+    def __iter__(self):
+        return iter(self.f)
