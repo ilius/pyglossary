d4d0b9efbe88c818fc4c07a7ba894fa2f5bdce26
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-13 23:48:10 +0430

    pep-8 fixes in plugins/octopus_mdict.py, add myself to copyright message

--- pyglossary/plugins/octopus_mdict.py
+++ pyglossary/plugins/octopus_mdict.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
-## octopus_mdic.py
-## Read Octopus MDict dictionary format, mdx(dictionary)/mdd(data)
-##
-## Copyright (C) 2013 Xiaoqiang Wang <xiaoqiangwang AT gmail DOT com>
-##
-## This program is a free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, version 3 of the License.
-##
-## You can get a copy of GNU General Public License along this program
-## But you can always get it from http://www.gnu.org/licenses/gpl.txt
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-## GNU General Public License for more details.
+# octopus_mdic.py
+# Read Octopus MDict dictionary format, mdx(dictionary)/mdd(data)
+#
+# Copyright (C) 2013 Xiaoqiang Wang <xiaoqiangwang AT gmail DOT com>
+# Copyright (C) 2013-2016 Saeed Rasooli <saeed.gnu@gmail.com>
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# You can get a copy of GNU General Public License along this program
+# But you can always get it from http://www.gnu.org/licenses/gpl.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 
 from formats_common import *
 
@@ -26,16 +27,18 @@ format = 'OctopusMdict'
 description = 'Octopus MDict'
 extentions = ['.mdx']
 readOptions = [
-    'resPath',## str, directory path
-    'encoding',## str
-    'substyle',## bool
+    'resPath',  # str, directory path
+    'encoding',  # str
+    'substyle',  # bool
 ]
 writeOptions = []
+
 
 class Reader(object):
     def __init__(self, glos):
         self._glos = glos
         self.clear()
+
     def clear(self):
         self._filename = ''
         self._encoding = ''
@@ -44,22 +47,23 @@ class Reader(object):
         self._mdd = None
         self._mddFilename = ''
         self._dataDir = ''
+
     def open(self, filename, **options):
         from pyglossary.plugin_lib.readmdict import MDX, MDD
         self._filename = filename
         self._encoding = options.get('encoding', '')
         self._substyle = options.get('substyle', True)
         self._mdx = MDX(filename, self._encoding, self._substyle)
-        ###
+
         filenameNoExt, ext = splitext(self._filename)
         self._dataDir = options.get('resPath', filenameNoExt + '_files')
         mddFilename = ''.join([filenameNoExt, extsep, 'mdd'])
         if isfile(mddFilename):
             self._mdd = MDD(mddFilename)
             self._mddFilename = mddFilename
-        ###
+
         log.pretty(self._mdx.header, 'mdx.header=')
-        #for key, value in self._mdx.header.items():
+        # for key, value in self._mdx.header.items():
         #    key = key.lower()
         #    self._glos.setInfo(key, value)
         try:
@@ -68,8 +72,11 @@ class Reader(object):
             pass
         else:
             self._glos.setInfo('title', title)
-        self._glos.setInfo('description', self._mdx.header.get(b'Description', ''))
-        ###
+        self._glos.setInfo(
+            'description',
+            self._mdx.header.get(b'Description', ''),
+        )
+
         try:
             self.writeDataFiles()
         except:
@@ -82,10 +89,10 @@ class Reader(object):
             os.makedirs(self._dataDir)
         for key, value in self._mdd.items():
             key = toStr(key)
-            fpath = ''.join([self._dataDir, key.replace('\\', os.path.sep)]);
+            fpath = ''.join([self._dataDir, key.replace('\\', os.path.sep)])
             if not isdir(dirname(fpath)):
                 os.makedirs(dirname(fpath))
-            log.debug('saving MDict data file: %s'%fpath)
+            log.debug('saving MDict data file: %s' % fpath)
             f = open(fpath, 'wb')
             f.write(value)
             f.close()
@@ -111,5 +118,3 @@ class Reader(object):
 
     def close(self):
         self.clear()
-
-
