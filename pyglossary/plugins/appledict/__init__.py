39f1069ca6529f6d146bf658c8721c8b586e9186
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-17 08:40:53 +0430

    AppleDict: update copyright year of Xiaoqiang Wang to 2012-2015 based on commits

--- pyglossary/plugins/appledict/__init__.py
+++ pyglossary/plugins/appledict/__init__.py
@@ -2,7 +2,7 @@
 # appledict/__init__.py
 # Output to Apple Dictionary xml sources for Dictionary Development Kit.
 #
-# Copyright (C) 2012 Xiaoqiang Wang <xiaoqiangwang AT gmail DOT com>
+# Copyright (C) 2012-2015 Xiaoqiang Wang <xiaoqiangwang AT gmail DOT com>
 #
 # This program is a free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
