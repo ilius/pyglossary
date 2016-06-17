73a59a77a033c0f82b5345fa458e1275fc9bb6e0
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-17 07:28:09 +0430

    pep-8 fixes in plugins/testformat.py

--- pyglossary/plugins/testformat.py
+++ pyglossary/plugins/testformat.py
@@ -9,35 +9,36 @@ extentions = ['.test', '.tst']
 readOptions = []
 writeOptions = []
 
-def read(glos, filename): ## glos is a Glossary object, filename is a string
-    log.info('reading from format %s using plugin'%format)
-    count = 100 ## get number of entries from input file(depending on your format)
+
+def read(glos, filename):  # glos is a Glossary object, filename is a string
+    log.info('reading from format %s using plugin' % format)
+    count = 100
+    # get number of entries from input file(depending on your format)
     for i in range(count):
-        ## here get word and definition from file(depending on your format)
-        word = 'word_%s'%i
-        defi = 'definition %s'%i
+        # here get word and definition from file(depending on your format)
+        word = 'word_%s' % i
+        defi = 'definition %s' % i
         glos.addEntry(word, defi)
-    ## here read info from file and set to Glossary object
+    # here read info from file and set to Glossary object
     glos.setInfo('name', 'Test')
     glos.setInfo('descriptin', 'Test glossary craeted by a PyGlossary plugin')
     glos.setInfo('author', 'Me')
     glos.setInfo('copyright', 'GPL')
-    return True ## reading input file was succesfull
+    return True  # reading input file was succesfull
 
 
-def write(glos, filename): ## glos is a Glossary object, filename is a string
-    log.info('writing to format %s using plugin'%format)
+def write(glos, filename):  # glos is a Glossary object, filename is a string
+    log.info('writing to format %s using plugin' % format)
     for entry in glos:
         word = entry.getWord()
         defi = entry.getDefi()
-        ## here write word and defi to the output file (depending on your format)
-    ## here read info from Glossaey object
+        # here write word and defi to the output file (depending on
+        # your format)
+    # here read info from Glossaey object
     name = glos.getInfo('name')
     descriptin = glos.getInfo('descriptin')
     author = glos.getInfo('author')
     copyright = glos.getInfo('copyright')
-    ## if an info key doesn't exist, getInfo returns empty string
-    ## now write info to the output file (depending on your output format)
-    return True ## writing output file was succesfull
-
-
+    # if an info key doesn't exist, getInfo returns empty string
+    # now write info to the output file (depending on your output format)
+    return True  # writing output file was succesfull
