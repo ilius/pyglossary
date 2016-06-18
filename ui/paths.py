e550f9ef870dd35044bfe7fa6238d00c8a0c2be6
Saeed Rasooli <saeed.gnu@gmail.com>
2016-06-19 01:41:57 +0430

    pep-8 fixes in ui/paths.py

--- ui/paths.py
+++ ui/paths.py
@@ -4,10 +4,10 @@ import sys
 uiDir = ''
 
 if hasattr(sys, 'frozen'):
-   rootDir = dirname(sys.executable)
-   uiDir = join(rootDir, 'ui')
+    rootDir = dirname(sys.executable)
+    uiDir = join(rootDir, 'ui')
 else:
-   uiDir = dirname(realpath(__file__))
-   rootDir = dirname(uiDir)
+    uiDir = dirname(realpath(__file__))
+    rootDir = dirname(uiDir)
 
 resDir = join(rootDir, 'res')
