3376180f083f3132c52acce520e4b731602cabff
Saeed Rasooli <saeed.gnu@gmail.com>
2016-07-30 15:55:22 +0430

    lingoes_ldf.py: add Reader class, loadInfo is not implemented yet

--- pyglossary/plugins/lingoes_ldf.py
+++ pyglossary/plugins/lingoes_ldf.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from formats_common import *
+from pyglossary.text_reader import TextGlossaryReader
+from pyglossary.file_utils import fileCountLines
 
 enable = True
 format = 'LingoesLDF'
@@ -20,6 +22,62 @@ infoKeys = [
     'website',
     'copyright',
 ]
+
+
+class Reader(TextGlossaryReader):
+    def __len__(self):
+        if self._wordCount is None:
+            log.debug('Try not to use len(reader) as it takes extra time')
+            self._wordCount = fileCountLines(
+                self._filename,
+                newline='\n\n',
+            ) - self._leadingLinesCount
+        return self._wordCount
+
+    def isInfoWord(self, word):
+        if isinstance(word, str):
+            return word.startswith('#')
+        else:
+            return False
+
+    def fixInfoWord(self, word):
+        if isinstance(word, str):
+            return word.lstrip('#')
+        else:
+            return word
+
+    def loadInfo(self): # FIXME
+        pass
+
+    def nextPair(self):
+        if not self._file:
+            raise StopIteration
+        entryLines = []
+        while True:
+            line = self._file.readline()
+            if not line:
+                raise StopIteration
+            line = line.rstrip('\n\r')  # FIXME
+            if line:
+                entryLines.append(line)
+                continue
+
+            # now `line` is empty, process `entryLines`
+            if not entryLines:
+                return
+            if len(entryLines) < 2:
+                log.error(
+                    'invalid block near line %s' % fileObj.line +
+                    ' in file %s' % filename
+                )
+                return
+            word = entryLines[0]
+            defi = '\n'.join(entryLines[1:])
+            defi = defi.replace('<br/>', '\n')  # FIXME
+
+            word = [p.strip() for p in word.split('|')]
+
+            return word, defi
 
 
 def read(glos, filename):
