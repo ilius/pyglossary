fe4622ffa9df5fda8627321a31709eefe7b455b2
Saeed Rasooli <saeed.gnu@gmail.com>
2018-09-15 09:45:55 +0800

    cc_cedict: change indentations to tabs (except jinja2htmlcompress.py), #141

--- pyglossary/plugins/cc_cedict/__init__.py
+++ pyglossary/plugins/cc_cedict/__init__.py
@@ -9,50 +9,50 @@ extentions = (".u8",)
 
 entry_count_reg = re.compile(r"#! entries=(\d+)")
 class Reader:
-    def __init__(self, glos):
-        self._glos = glos
-        self.file = None
-        self.total_entries = self.entries_left = None
+	def __init__(self, glos):
+		self._glos = glos
+		self.file = None
+		self.total_entries = self.entries_left = None
 
-    def open(self, filename, encoding="utf-8"):
-        if self.file is not None:
-            self.file.close()
-        self.file = open(filename, "r", encoding=encoding)
-        for line in self.file:
-            match = entry_count_reg.match(line)
-            if match is not None:
-                count = match.groups()[0]
-                self.total_entries = self.entries_left = int(count)
-                break
-        else:
-            self.close()
-            raise RuntimeError("CC-CEDICT: could not find entry count")
+	def open(self, filename, encoding="utf-8"):
+		if self.file is not None:
+			self.file.close()
+		self.file = open(filename, "r", encoding=encoding)
+		for line in self.file:
+			match = entry_count_reg.match(line)
+			if match is not None:
+				count = match.groups()[0]
+				self.total_entries = self.entries_left = int(count)
+				break
+		else:
+			self.close()
+			raise RuntimeError("CC-CEDICT: could not find entry count")
 
-    def close(self):
-        if self.file is not None:
-            self.file.close()
-        self.file = None
-        self.total_entries = self.entries_left = None
+	def close(self):
+		if self.file is not None:
+			self.file.close()
+		self.file = None
+		self.total_entries = self.entries_left = None
 
-    def __len__(self):
-        if self.total_entries is None:
-            raise RuntimeError("CC-CEDICT: len(reader) called "\
-                    "while reader is not open")
-        return self.total_entries
+	def __len__(self):
+		if self.total_entries is None:
+			raise RuntimeError("CC-CEDICT: len(reader) called "\
+					"while reader is not open")
+		return self.total_entries
 
-    def __iter__(self):
-        if self.file is None:
-            raise RuntimeError("CC-CEDICT: tried to iterate over entries "\
-                    "while reader is not open")
-        for line in self.file:
-            if not line.startswith("#"):
-                if self.entries_left == 0:
-                    log.warning("more entries than the header claimed?!")
-                self.entries_left -= 1
-                parts = conv.parse_line(line)
-                if parts is None:
-                    log.warning("bad line: %s", line)
-                    continue
-                names, article = conv.make_entry(*parts)
-                entry = self._glos.newEntry(names, article, defiFormat="h")
-                yield entry
+	def __iter__(self):
+		if self.file is None:
+			raise RuntimeError("CC-CEDICT: tried to iterate over entries "\
+					"while reader is not open")
+		for line in self.file:
+			if not line.startswith("#"):
+				if self.entries_left == 0:
+					log.warning("more entries than the header claimed?!")
+				self.entries_left -= 1
+				parts = conv.parse_line(line)
+				if parts is None:
+					log.warning("bad line: %s", line)
+					continue
+				names, article = conv.make_entry(*parts)
+				entry = self._glos.newEntry(names, article, defiFormat="h")
+				yield entry
--- pyglossary/plugins/cc_cedict/conv.py
+++ pyglossary/plugins/cc_cedict/conv.py
@@ -7,36 +7,36 @@ import jinja2
 
 line_reg = re.compile(r"^([^ ]+) ([^ ]+) \[([^\]]+)\] /(.+)/$")
 def parse_line(line):
-    line = line.strip()
-    match = line_reg.match(line)
-    if match is None: return None
-    trad, simp, pinyin, eng = match.groups()
-    pinyin = pinyin.replace("u:", "v")
-    eng = eng.split("/")
-    return trad, simp, pinyin, eng
+	line = line.strip()
+	match = line_reg.match(line)
+	if match is None: return None
+	trad, simp, pinyin, eng = match.groups()
+	pinyin = pinyin.replace("u:", "v")
+	eng = eng.split("/")
+	return trad, simp, pinyin, eng
 
 def make_entry(trad, simp, pinyin, eng):
-    eng_names = list(map(summarize, eng))
-    names = [simp, trad, pinyin] + eng_names
-    article = render_article(trad, simp, pinyin, eng)
-    return names, article
+	eng_names = list(map(summarize, eng))
+	names = [simp, trad, pinyin] + eng_names
+	article = render_article(trad, simp, pinyin, eng)
+	return names, article
 
 script_dir = os.path.dirname(__file__)
 jinja_env = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(script_dir),
-    extensions=[HTMLCompress])
+	loader=jinja2.FileSystemLoader(script_dir),
+	extensions=[HTMLCompress])
 COLORS = {"": "black", "1": "red", "2": "orange", "3": "green", "4": "blue", "5": "black"}
 def render_article(trad, simp, pinyin, eng):
-    pinyin_tones = [convert(syl) for syl in pinyin.split()]
-    nice_pinyin = []
-    tones = []
-    for syllable in pinyin.split():
-        nice_syllable, tone = convert(syllable)
-        nice_pinyin.append(nice_syllable)
-        tones.append(tone)
-    args = dict(COLORS=COLORS, trad=trad, simp=simp,
-        pinyin=nice_pinyin, tones=tones, defns=eng)
+	pinyin_tones = [convert(syl) for syl in pinyin.split()]
+	nice_pinyin = []
+	tones = []
+	for syllable in pinyin.split():
+		nice_syllable, tone = convert(syllable)
+		nice_pinyin.append(nice_syllable)
+		tones.append(tone)
+	args = dict(COLORS=COLORS, trad=trad, simp=simp,
+		pinyin=nice_pinyin, tones=tones, defns=eng)
 
-    template = jinja_env.get_template("article.html")
-    return template.render(zip=zip, **args)
+	template = jinja_env.get_template("article.html")
+	return template.render(zip=zip, **args)
 
