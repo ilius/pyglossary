737aaefa06ad92d4680de6d076cb9a82e9836510
Saeed Rasooli <saeed.gnu@gmail.com>
2019-06-20 21:12:11 +0430

    octopus_mdict_source.py: fix #68, add support for inconsecutive links with --read-options=links=True

--- pyglossary/plugins/octopus_mdict_source.py
+++ pyglossary/plugins/octopus_mdict_source.py
@@ -2,6 +2,7 @@
 # http://www.octopus-studio.com/download.en.htm
 
 from formats_common import *
+from collections import OrderedDict
 
 enable = True
 format = "OctopusMdictSource"
@@ -9,6 +10,7 @@ description = "Octopus MDict Source"
 extentions = [".mtxt"]
 readOptions = [
 	"encoding",  # str
+	"links", # bool, support for inconsecutive links, #68
 ]
 writeOptions = [
 	"resources",  # bool
@@ -19,12 +21,15 @@ def read(
 	glos,
 	filename,
 	encoding="utf-8",
+	links=False,
 ):
 	with open(filename, encoding=encoding) as fp:
 		text = fp.read()
 	text = text.replace("\r\n", "\n")
 	text = text.replace("entry://", "bword://")
 	lastEntry = None
+	linksDict = {}
+	entryDict = OrderedDict()
 	for section in text.split("</>"):
 		lines = section.strip().split("\n")
 		if len(lines) < 2:
@@ -33,25 +38,39 @@ def read(
 		defi = "\n".join(lines[1:])
 
 		if defi.startswith("@@@LINK="):
-			if not lastEntry:
-				log.error("alternate section not after a word: %s" % defi)
-				continue
 			mainWord = defi.partition("=")[2]
-			if lastEntry.getWords()[0] != mainWord:
+			if links:
+				linksDict[word] = mainWord
+			elif lastEntry and lastEntry.getWords()[0] == mainWord:
+				lastEntry.addAlt(word)
+			else:
 				log.error("alternate is not ride after word: %s" % defi)
-				continue
-			lastEntry.addAlt(word)
 			continue
 
-		# now that we know there are no more alternate forms of lastEntry
+		entry = glos.newEntry(word, defi)
+		if links:
+			# do not call glos.addEntry or glos.addEntryObj
+			# because we will need to modify entries at the end
+			# and we need to keep a OrderedDict of entries (ordered list of entries, and dict of entries by word)
+			entryDict[word] = entry
+		else:
+			if lastEntry:
+				# now that we know there are no more alternate forms of lastEntry
+				glos.addEntryObj(lastEntry)
+			lastEntry = entry
+
+	if links:
+		for sourceWord, targetWord in linksDict.items():
+			targetEntry = entryDict.get(targetWord)
+			if targetEntry is None:
+				log.error("Link to non-existing word %s" % targetWord)
+				continue
+			targetEntry.addAlt(sourceWord)
+		for entry in entryDict.values():
+			glos.addEntryObj(entry)
+	else:
 		if lastEntry:
 			glos.addEntryObj(lastEntry)
-		entry = glos.newEntry(word, defi)
-		lastEntry = entry
-
-	if lastEntry:
-		glos.addEntryObj(lastEntry)
-
 
 def writeEntryGen(glos):
 	for entry in glos:
