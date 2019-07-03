b585addbaf0d576401b6bd3bd9614f97f3b947b3
Saeed Rasooli <saeed.gnu@gmail.com>
2019-07-03 16:05:32 +0430

    add types to pyglossary/entry.py

--- pyglossary/entry.py
+++ pyglossary/entry.py
@@ -7,15 +7,32 @@ from os.path import (
 	exists,
 	dirname,
 )
+from typing import (
+	Optional,
+	Union,
+	Tuple,
+	List,
+	Dict,
+	Callable,
+	Any,
+)
 
 
 from .entry_base import BaseEntry
+
+MultiStr = Union[str, List[str]]
+
+RawEntryType = Union[
+	Tuple[MultiStr, MultiStr],
+	Tuple[MultiStr, MultiStr, str],
+]
+
 
 class DataEntry(BaseEntry): # or Resource? FIXME
 	def isData(self):
 		return True
 
-	def __init__(self, fname, data, inTmp=False):
+	def __init__(self, fname: str, data: bytes, inTmp: bool = False) -> None:
 		assert isinstance(fname, str)
 		assert isinstance(data, bytes)
 		assert isinstance(inTmp, bool)
@@ -32,17 +49,17 @@ class DataEntry(BaseEntry): # or Resource? FIXME
 		self._data = data  # bytes instance
 		self._tmpPath = tmpPath
 
-	def getFileName(self):
+	def getFileName(self) -> str:
 		return self._fname
 
-	def getData(self):
+	def getData(self) -> bytes:
 		if self._tmpPath:
 			with open(self._tmpPath, "rb") as fromFile:
 				return fromFile.read()
 		else:
 			return self._data
 
-	def save(self, directory):
+	def save(self, directory: str) -> str:
 		fname = self._fname
 		# fix filename depending on operating system? FIXME
 		fpath = join(directory, fname)
@@ -53,16 +70,16 @@ class DataEntry(BaseEntry): # or Resource? FIXME
 			toFile.write(self.getData())
 		return fpath
 
-	def getWord(self):
+	def getWord(self) -> str:
 		return self._fname
 
-	def getWords(self):
+	def getWords(self) -> List[str]:
 		return [self._fname]
 
-	def getDefi(self):
+	def getDefi(self) -> str:
 		return "File: %s" % self._fname  
 
-	def getDefis(self):
+	def getDefis(self) -> List[str]:
 		return [self.getDefi()]
 
 	def getDefiFormat(self):
@@ -71,33 +88,33 @@ class DataEntry(BaseEntry): # or Resource? FIXME
 	def setDefiFormat(self, defiFormat):
 		pass
 
-	def detectDefiFormat(self):
+	def detectDefiFormat(self) -> None:
 		pass
 
-	def addAlt(self, alt):
+	def addAlt(self, alt: str) -> None:
 		pass
 
-	def editFuncWord(self, func):
+	def editFuncWord(self, func: Callable[[str], str]) -> None:
 		pass
 		# modify fname?
 		# FIXME
 
-	def editFuncDefi(self, func):
+	def editFuncDefi(self, func: Callable[[str], str]) -> None:
 		pass
 
-	def strip(self):
+	def strip(self) -> None:
 		pass
 
-	def replaceInWord(self, source, target):
+	def replaceInWord(self, source: str, target: str) -> None:
 		pass
 
-	def replaceInDefi(self, source, target):
+	def replaceInDefi(self, source: str, target: str) -> None:
 		pass
 
-	def replace(self, source, target):
+	def replace(self, source: str, target: str) -> None:
 		pass
 
-	def getRaw(self):
+	def getRaw(self) -> Tuple[str, str]:
 		return (
 			self._fname,
 			"DATA",
@@ -117,24 +134,28 @@ class Entry(BaseEntry):
 		re.S,
 	)
 
-	def isData(self):
+	def isData(self) -> bool:
 		return False
 
-	def _join(self, parts):
+	def _join(self, parts: List[str]) -> str:
 		return self.sep.join([
 			part.replace(self.sep, "\\"+self.sep)
 			for part in parts
 		])
 
 	@staticmethod
-	def getEntrySortKey(key=None):
+	def getEntrySortKey(key: Callable[[str], Any] = None) -> Callable[[BaseEntry], Any]:
 		if key:
 			return lambda entry: key(entry.getWords()[0])
 		else:
 			return lambda entry: entry.getWords()[0]
 
 	@staticmethod
-	def getRawEntrySortKey(key=None):
+	def getRawEntrySortKey(key: Callable[[str], Any] = None) -> Callable[[Tuple], str]:
+		# here `x` is raw entity, meaning a tuple of form (word, defi) or (word, defi, defiFormat)
+		# so x[0] is word(s), that can be a str (one word),
+		# or a list or tuple (one word with or more alternaties)
+		# FIXME: drop the case that x[0] is tuple
 		if key:
 			return lambda x: key(
 				x[0][0] if isinstance(x[0], (list, tuple)) else x[0]
@@ -143,7 +164,12 @@ class Entry(BaseEntry):
 			return lambda x: \
 				x[0][0] if isinstance(x[0], (list, tuple)) else x[0]
 
-	def __init__(self, word, defi, defiFormat="m"):
+	def __init__(
+		self,
+		word: MultiStr,
+		defi: MultiStr,
+		defiFormat: str = "m",
+	) -> None:
 		"""
 			word: string or a list of strings (including alternate words)
 			defi: string or a list of strings (including alternate definitions)
@@ -173,7 +199,7 @@ class Entry(BaseEntry):
 		self._defi = defi
 		self._defiFormat = defiFormat
 
-	def getWord(self):
+	def getWord(self) -> str:
 		"""
 			returns string of word,
 				and all the alternate words
@@ -184,7 +210,7 @@ class Entry(BaseEntry):
 		else:
 			return self._join(self._word)
 
-	def getWords(self):
+	def getWords(self) -> List[str]:
 		"""
 			returns list of the word and all the alternate words
 		"""
@@ -193,7 +219,7 @@ class Entry(BaseEntry):
 		else:
 			return self._word
 
-	def getDefi(self):
+	def getDefi(self) -> str:
 		"""
 			returns string of definition,
 				and all the alternate definitions
@@ -204,7 +230,7 @@ class Entry(BaseEntry):
 		else:
 			return self._join(self._defi)
 
-	def getDefis(self):
+	def getDefis(self) -> List[str]:
 		"""
 			returns list of the definition and all the alternate definitions
 		"""
@@ -222,7 +248,7 @@ class Entry(BaseEntry):
 		"""
 		return self._defiFormat
 
-	def setDefiFormat(self, defiFormat):
+	def setDefiFormat(self, defiFormat) -> str:
 		"""
 			defiFormat:
 				"m": plain text
@@ -231,19 +257,19 @@ class Entry(BaseEntry):
 		"""
 		self._defiFormat = defiFormat
 
-	def detectDefiFormat(self):
+	def detectDefiFormat(self) -> None:
 		if self._defiFormat != "m":
 			return
 		defi = self.getDefi().lower()
 		if re.match(self.htmlPattern, defi):
 			self._defiFormat = "h"
 
-	def addAlt(self, alt):
+	def addAlt(self, alt: str):
 		words = self.getWords()
 		words.append(alt)
 		self._word = words
 
-	def editFuncWord(self, func):
+	def editFuncWord(self, func: Callable[[str], str]) -> None:
 		"""
 			run function `func` on all the words
 			`func` must accept only one string as argument
@@ -256,7 +282,7 @@ class Entry(BaseEntry):
 				func(st) for st in self._word
 			)
 
-	def editFuncDefi(self, func):
+	def editFuncDefi(self, func: Callable[[str], str]) -> None:
 		"""
 			run function `func` on all the definitions
 			`func` must accept only one string as argument
@@ -269,14 +295,14 @@ class Entry(BaseEntry):
 				func(st) for st in self._defi
 			)
 
-	def strip(self):
+	def strip(self) -> None:
 		"""
 			strip whitespaces from all words and definitions
 		"""
 		self.editFuncWord(str.strip)
 		self.editFuncDefi(str.strip)
 
-	def replaceInWord(self, source, target):
+	def replaceInWord(self, source: str, target: str) -> None:
 		"""
 			replace string `source` with `target` in all words
 		"""
@@ -287,7 +313,7 @@ class Entry(BaseEntry):
 				st.replace(source, target) for st in self._word
 			)
 
-	def replaceInDefi(self, source, target):
+	def replaceInDefi(self, source: str, target: str) -> None:
 		"""
 			replace string `source` with `target` in all definitions
 		"""
@@ -298,14 +324,14 @@ class Entry(BaseEntry):
 				st.replace(source, target) for st in self._defi
 			)
 
-	def replace(self, source, target):
+	def replace(self, source: str, target: str) -> None:
 		"""
 			replace string `source` with `target` in all words and definitions
 		"""
 		self.replaceInWord(source, target)
 		self.replaceInDefi(source, target)
 
-	def getRaw(self):
+	def getRaw(self) -> RawEntryType:
 		"""
 			returns a tuple (word, defi) or (word, defi, defiFormat)
 			where both word and defi might be string or list of strings
@@ -323,7 +349,7 @@ class Entry(BaseEntry):
 			)
 
 	@classmethod
-	def fromRaw(cls, rawEntry, defaultDefiFormat="m"):
+	def fromRaw(cls, rawEntry: RawEntryType, defaultDefiFormat: str = "m"):
 		"""
 			rawEntry can be (word, defi) or (word, defi, defiFormat)
 			where both word and defi can be string or list of strings
