6209a999196ab3bb4c42ca2d271e35bed62476be
Saeed Rasooli <saeed.gnu@gmail.com>
2020-03-08 11:29:39 +0800

    appledict_bin.py: add read option html=False, some refactoring and pep8 fixes

--- pyglossary/plugins/appledict_bin.py
+++ pyglossary/plugins/appledict_bin.py
@@ -18,13 +18,16 @@ from formats_common import *
 
 from struct import unpack
 from zlib import decompress
+from lxml import etree
 
 enable = True
 format = "AppleDictBin"
 description = "AppleDict Binary (.dictionary)"
 extensions = [".dictionary", ".data"]
+optionsProp = {
+	"html": BoolOption(),
+}
 
-from lxml import etree
 
 class Reader(object):
 	def __init__(self, glos):
@@ -32,8 +35,11 @@ class Reader(object):
 		self._filename = ""
 		self._file = None
 		self._encoding = "utf-8"
+		self._buf = ""
+		self._html = False
 
-	def open(self, filename):
+	def open(self, filename, html: bool = False):
+		self._html = html
 		parts = filename.split(os.sep)
 		dbname = parts[-1]
 		if isdir(filename):
@@ -47,9 +53,9 @@ class Reader(object):
 			dbname = parts[-2]
 			if len(parts) > 2:
 				if dbname == "Contents":
-					dbname =  parts[-3]
+					dbname = parts[-3]
 				elif dbname == "Resources" and len(parts) > 3:
-					dbname =  parts[-4]
+					dbname = parts[-4]
 
 		if dbname.endswith(".dictionary"):
 			dbname = dbname[:-len(".dictionary")]
@@ -74,19 +80,59 @@ class Reader(object):
 	def decode(self, st: bytes) -> str:
 		return st.decode(self._encoding, errors="replace")
 
-	def getChunkSize(self, buf, pos):
-		plus = buf[pos:pos+12].find(b"<d:entry")
+	def getChunkSize(self, pos):
+		plus = self._buf[pos:pos + 12].find(b"<d:entry")
 		if plus < 1:
 			return 0, 0
-		bs = buf[pos:pos+plus]
+		bs = self._buf[pos:pos + plus]
 		if plus < 4:
 			bs = b"\x00" * (4 - plus) + bs
 		try:
 			chunkSize, = unpack("i", bs)
 		except Exception as e:
-			log.error(f"{buf[pos:pos+100]}")
+			log.error(f"{self._buf[pos:pos+100]}")
 			raise e
 		return chunkSize, plus
+
+	def _readEntry(self, pos: int) -> Tuple["BaseEntry", int]:
+		"""
+			returns (entry, pos)
+		"""
+		chunkSize, plus = self.getChunkSize(pos)
+		pos += plus
+		if chunkSize == 0:
+			endI = self._buf[pos:].find(b"</d:entry>")
+			if endI == -1:
+				chunkSize = len(self._buf) - pos
+			else:
+				chunkSize = endI + 10
+		entryFull = self.decode(self._buf[pos:pos + chunkSize])
+		entryFull = entryFull.strip()
+		if not entryFull:
+			pos += chunkSize
+			return None, pos
+		try:
+			entryRoot = etree.fromstring(entryFull)
+		except etree.XMLSyntaxError as e:
+			log.error(f"\n{self._buf[pos-plus:pos+100]}")
+			log.error(
+				f"chunkSize={chunkSize}, plus={plus}, pos={pos}, len(buf)={len(self._buf)}"
+			)
+			log.error(f"entryFull={entryFull!r}")
+			raise e
+		entryElems = entryRoot.xpath("/d:entry", namespaces=entryRoot.nsmap)
+		if not entryElems:
+			return None, pos
+		word = entryElems[0].xpath("./@d:title", namespaces=entryRoot.nsmap)[0]
+		if self._html:
+			defi = self.decode(etree.tostring(entryElems[0]))
+		else:
+			defi = "".join([
+				self.decode(etree.tostring(child))
+				for child in entryElems[0].iterdescendants()
+			])
+		pos += chunkSize
+		return self._glos.newEntry(word, defi), pos
 
 	def __iter__(self):
 		file = self._file
@@ -97,54 +143,23 @@ class Reader(object):
 			# ~ flag = f.tell()
 			# ~ bufSize = 0
 			# ~ while True:
-				# ~ zipp = f.read(bufSize)
-				# ~ try:
-					# ~ # print(zipp)
-					# ~ input(zipp.decode(self._encoding))
-					# ~ buf = decompress(zipp[8:])
-					# ~ # print(buf)
-					# ~ break
-				# ~ except:
-					# ~ print(bufSize)
-					# ~ f.seek(flag)
-					# ~ bufSize = bufSize+1
+			# ~ 	zipp = f.read(bufSize)
+			# ~		try:
+			# ~			# print(zipp)
+			# ~			input(zipp.decode(self._encoding))
+			# ~			buf = decompress(zipp[8:])
+			# ~			# print(buf)
+			# ~			break
+			# ~		except:
+			# ~			print(bufSize)
+			# ~			f.seek(flag)
+			# ~			bufSize = bufSize+1
 
 			bufSize, = unpack("i", bufSizeB)  # type: int
-			buf = decompress(file.read(bufSize)[8:])
+			self._buf = decompress(file.read(bufSize)[8:])
 
 			pos = 0
-			while pos < len(buf):
-				chunkSize, plus = self.getChunkSize(buf, pos)
-				pos += plus
-				if chunkSize == 0:
-					endI = buf[pos:].find(b"</d:entry>")
-					if endI == -1:
-						chunkSize = len(buf) - pos
-					else:
-						chunkSize = endI + 10
-				entryFull = self.decode(buf[pos:pos+chunkSize])
-				entryFull = entryFull.strip()
-				if not entryFull:
-					pos += chunkSize
-					continue
-				try:
-					entryRoot = etree.fromstring(entryFull)
-				except etree.XMLSyntaxError as e:
-					log.error(f"\n{buf[pos-plus:pos+100]}")
-					log.error(f"chunkSize={chunkSize}, plus={plus}, pos={pos}, len(buf)={len(buf)}")
-					log.error(f"entryFull={entryFull!r}")
-					raise e
-				entryElems = entryRoot.xpath("/d:entry", namespaces=entryRoot.nsmap)
-				if not entryElems:
-					continue
-				entryElem = entryElems[0]
-				word = entryElem.xpath("./@d:title", namespaces=entryRoot.nsmap)[0]
-				defi = "".join([
-					self.decode(etree.tostring(child))
-					for child in entryElem.iterdescendants()
-				])
-				yield self._glos.newEntry(
-					word,
-					defi,
-				)
-				pos += chunkSize
+			while pos < len(self._buf):
+				entry, pos = self._readEntry(pos)
+				if entry is not None:
+					yield entry
