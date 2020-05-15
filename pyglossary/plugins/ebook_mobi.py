537ac35b724c741b5e04ec32f4f3c5550fb2c249
Saeed Rasooli <saeed.gnu@gmail.com>
2020-05-15 13:13:38 +0430

    ebook_mobi.py: fix python2-style unicode strings

--- pyglossary/plugins/ebook_mobi.py
+++ pyglossary/plugins/ebook_mobi.py
@@ -41,10 +41,10 @@ optionsProp = {
 
 
 class Writer(EbookWriter):
-	ebook_format = u"mobi"
+	ebook_format = "mobi"
 
-	CSS_CONTENTS = u""""@charset "UTF-8";"""
-	GROUP_XHTML_TEMPLATE = u"""<?xml version="1.0" encoding="utf-8" standalone="no"?>
+	CSS_CONTENTS = """"@charset "UTF-8";"""
+	GROUP_XHTML_TEMPLATE = """<?xml version="1.0" encoding="utf-8" standalone="no"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
  <head>
@@ -62,13 +62,13 @@ class Writer(EbookWriter):
  </body>
 </html>"""
 
-	GROUP_XHTML_INDEX_LINK = u"""   <a href="index.xhtml">[ Index ]</a>"""
+	GROUP_XHTML_INDEX_LINK = """   <a href="index.xhtml">[ Index ]</a>"""
 
-	GROUP_XHTML_WORD_TEMPLATE = u"""   <span class="groupHeadword"><idx:entry><idx:orth>{headword}</idx:orth></idx:entry></span>"""
+	GROUP_XHTML_WORD_TEMPLATE = """   <span class="groupHeadword"><idx:entry><idx:orth>{headword}</idx:orth></idx:entry></span>"""
 
-	GROUP_XHTML_WORD_JOINER = u" &#8226;\n"
+	GROUP_XHTML_WORD_JOINER = " &#8226;\n"
 
-	GROUP_XHTML_WORD_DEFINITION_TEMPLATE = u"""  <div class="groupEntry">
+	GROUP_XHTML_WORD_DEFINITION_TEMPLATE = """  <div class="groupEntry">
    <idx:entry>
 	<h2 class="groupHeadword"><idx:orth>{headword}</idx:orth></h2>
 	<p class="groupDefinition">{definition}</p>
@@ -76,7 +76,7 @@ class Writer(EbookWriter):
   </div>"""
 
 
-	OPF_TEMPLATE = u"""<?xml version="1.0" encoding="utf-8"?>
+	OPF_TEMPLATE = """<?xml version="1.0" encoding="utf-8"?>
 <package unique-identifier="uid">
  <metadata>
   <dc-metadata xmlns:dc="http://purl.org/metadata/dublin_core" xmlns:oebpackage="http://openebook.org/namespaces/oeb-package/1.0/">
@@ -144,7 +144,7 @@ class Writer(EbookWriter):
 				print_debug("Creating .mobi file with kindlegen...", args.debug)
 				kindlegen_path = KINDLEGEN
 				opf_file_path_absolute = os.path.join(tmp_path, "OEBPS", "content.opf")
-				mobi_file_path_relative = u"content.mobi"
+				mobi_file_path_relative = "content.mobi"
 				mobi_file_path_absolute = os.path.join(tmp_path, "OEBPS", mobi_file_path_relative)
 				if args.kindlegen_path is None:
 					print_info("  Running '%s' from $PATH" % KINDLEGEN)
