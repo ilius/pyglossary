dcfe70fb86a17c0d68e508b4a52b0cf3b3d764a0
Saeed Rasooli <saeed.gnu@gmail.com>
2020-05-23 15:25:46 +0430

    add copyright block to 3 files in ui/gtk3_utils/

--- ui/gtk3_utils/resize_button.py
+++ ui/gtk3_utils/resize_button.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2016-2017 Saeed Rasooli <saeed.gnu@gmail.com> (ilius)
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# You can get a copy of GNU General Public License along this program
+# But you can always get it from http://www.gnu.org/licenses/gpl.txt
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+
 from . import *
 from .utils import *
 
