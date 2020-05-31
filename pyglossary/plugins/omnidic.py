8b2ac496dd1c18eeb8818aeb6e00d8732e945c6a
Saeed Rasooli <saeed.gnu@gmail.com>
2020-05-31 06:40:16 +0430

    omnidic.py: f-string, double-quote and refactoring

--- pyglossary/plugins/omnidic.py
+++ pyglossary/plugins/omnidic.py
@@ -3,9 +3,9 @@
 from formats_common import *
 
 enable = True
-format = 'Omnidic'
-description = 'Omnidic'
-extensions = ['.omni', '.omnidic']
+format = "Omnidic"
+description = "Omnidic"
+extensions = [".omni", ".omnidic"]
 optionsProp = {
 	"dicIndex": IntOption(),
 }
@@ -14,33 +14,24 @@ depends = {}
 
 def write(glos: GlossaryType, filename: str, dicIndex=16):
 	if not isinstance(dicIndex, int):
-		raise TypeError(
-			'invalid dicIndex=%r, must be integer' % dicIndex
-		)
+		raise TypeError(f"invalid dicIndex={dicIndex!r}, must be integer")
 	with indir(filename, create=True):
-		indexFp = open(str(dicIndex), 'w')
+		indexFp = open(str(dicIndex), "w", encoding="utf8")
 
 		for bucketIndex, bucket in enumerate(glos.iterEntryBuckets(100)):
-			if bucketIndex == 0:
-				bucketFilename = '%s99' % dicIndex
-			else:
-				bucketFilename = '%s%s' % (
-					dicIndex,
-					bucketIndex * 100 + len(bucket) - 1,
-				)
+			lastWordIndex = bucketIndex * 100 + len(bucket) - 1
+			bucketFilename = f"{dicIndex}{lastWordIndex}"
 
-			indexFp.write('%s#%s#%s\n' % (
-				bucket[0].getWord(),
-				bucket[-1].getWord(),
-				bucketFilename,
-			))
+			firstWord = bucket[0].getWord()
+			lastWord = bucket[-1].getWord()
+			indexFp.write(f"{firstWord}#{lastWord}#{bucketFilename}\n")
 
-			bucketFileObj = open(bucketFilename, 'w')
+			bucketFileObj = open(bucketFilename, "w", encoding="utf8")
 			for entry in bucket:
 				word = entry.getWord()
 				defi = entry.getDefi()
-				defi = defi.replace('\n', '  ')  # FIXME
-				bucketFileObj.write('%s#%s\n' % (word, defi))
+				defi = defi.replace("\n", "  ")  # FIXME
+				bucketFileObj.write(f"{word}#{defi}\n")
 			bucketFileObj.close()
 
 		indexFp.close()
