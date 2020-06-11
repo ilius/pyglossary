4baa41a25966dae88f75bc9ece65324e71bb084a
Saeed Rasooli <saeed.gnu@gmail.com>
2020-06-12 01:26:57 +0430

    json_utils.py: remove unused functions, and switch to f-string

--- pyglossary/json_utils.py
+++ pyglossary/json_utils.py
@@ -18,6 +18,7 @@ from types import ModuleType
 JsonEncodable = Union[Dict, List]
 # OrderedDict is also subclass of Dict, issubclass(OrderedDict, Dict) == True
 
+
 def dataToPrettyJson(
 	data: JsonEncodable,
 	ensure_ascii: bool = False,
@@ -52,91 +53,3 @@ def jsonToOrderedData(text: str) -> OrderedDict:
 	return json.JSONDecoder(
 		object_pairs_hook=OrderedDict,
 	).decode(text)
-
-###############################
-
-
-def loadJsonConf(module: Union[ModuleType, str], confPath: str, decoders: Optional[Dict] = None) -> None:
-	from os.path import isfile
-	###
-	if not isfile(confPath):
-		return
-	###
-	try:
-		text = open(confPath).read()
-	except Exception as e:
-		print('failed to read file "%s": %s' % (confPath, e))
-		return
-	###
-	try:
-		data = json.loads(text)
-	except Exception as e:
-		print('invalid json file "%s": %s' % (confPath, e))
-		return
-	###
-	if isinstance(module, str):
-		module = sys.modules[module]
-	for param, value in data.items():
-		if decoders:
-			decoder = decoders.get(param)
-			if decoder is not None:
-				value = decoder(value)
-		setattr(module, param, value)
-
-
-def saveJsonConf(module: Union[ModuleType, str], confPath: str, params: str, encoders: Optional[Dict] = None) -> None:
-	if isinstance(module, str):
-		module = sys.modules[module]
-	###
-	data = OrderedDict()
-	for param in params:
-		value = getattr(module, param)
-		if encoders:
-			encoder = encoders.get(param)
-			if encoder is not None:
-				value = encoder(value)
-		data[param] = value
-	###
-	text = dataToPrettyJson(data)
-	try:
-		open(confPath, 'w').write(text)
-	except Exception as e:
-		print('failed to save file "%s": %s' % (confPath, e))
-		return
-
-
-def loadModuleJsonConf(module: Union[ModuleType, str]) -> None:
-	if isinstance(module, str):
-		module = sys.modules[module]
-	###
-	decoders = getattr(module, 'confDecoders', {})
-	###
-	try:
-		sysConfPath = module.sysConfPath
-	except AttributeError:
-		pass
-	else:
-		loadJsonConf(
-			module,
-			sysConfPath,
-			decoders,
-		)
-	####
-	loadJsonConf(
-		module,
-		module.confPath,
-		decoders,
-	)
-	# should use module.confParams to restrict json keys? FIXME
-
-
-def saveModuleJsonConf(module: Union[ModuleType, str]) -> None:
-	if isinstance(module, str):
-		module = sys.modules[module]
-	###
-	saveJsonConf(
-		module,
-		module.confPath,
-		module.confParams,
-		getattr(module, 'confEncoders', {}),
-	)
