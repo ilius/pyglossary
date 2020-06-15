36dfc46cb6f1ebfaf924ef51f20d97a48a3c5719
Saeed Rasooli <saeed.gnu@gmail.com>
2020-06-15 17:03:28 +0430

    Freedict: add read support, without progressbar / __len__, issue #206

--- pyglossary/plugins/freedict.py
+++ pyglossary/plugins/freedict.py
@@ -2,6 +2,9 @@
 
 from formats_common import *
 from pyglossary.xml_utils import xml_escape
+from typing import List, Union, Callable
+from lxml import etree as ET
+from io import BytesIO
 
 enable = True
 format = "Freedict"
@@ -11,6 +14,111 @@ optionsProp = {
 	"resources": BoolOption(),
 }
 depends = {}
+
+tei = '{http://www.tei-c.org/ns/1.0}'
+
+class Reader(object):
+	ns = {
+		None: 'http://www.tei-c.org/ns/1.0',
+	}
+
+	def to_string(self, el):
+		return ET.tostring(el, method='html', pretty_print=True).decode('utf-8')
+
+	def make_list(
+		self,
+		hf: ET.htmlfile,
+		input_elements: List[ET.Element],
+		processor: Callable,
+		single_prefix=None,
+		skip_single=True
+	):
+		""" Wrap elements into <ol> if more than one element """
+		if len(input_elements) == 0:
+			return
+
+		if len(input_elements) == 1:
+			hf.write(single_prefix)
+			processor(hf, input_elements[0])
+			return
+
+		with hf.element('ol'):
+			for el in input_elements:
+				with hf.element('li'):
+					processor(hf, el)
+
+	def process_sense(self, hf: ET.htmlfile, sense: ET.Element):
+		# translations
+		hf.write(', '.join(
+			el.text
+			for el in sense.findall('cit/quote', self.ns)
+			if el.text is not None
+		))
+
+		self.make_list(
+			hf,
+			sense.findall('sense/def', self.ns),
+			lambda hf, el: hf.write(el.text),
+			single_prefix=' — ',
+		)
+
+	def get_entry_html(self, entry):
+		keywords = []
+		f = BytesIO()
+		with ET.htmlfile(f) as hf:
+			with hf.element('div'):
+				for form in entry.findall('form/orth', self.ns):
+					keywords.append(form.text)
+					with hf.element('b'):
+						hf.write(form.text)
+				hf.write(' ')
+				for pos in entry.findall('gramGrp/pos', self.ns):
+					with hf.element('i'):
+						hf.write(pos.text)
+				hf.write(ET.Element('br'))
+				hf.write('\n')
+
+				self.make_list(
+					hf,
+					entry.findall('sense', self.ns),
+					self.process_sense,
+				)
+
+		return keywords, f.getvalue().decode('utf8')
+
+	def set_metadata(self, header):
+		self._glos.setInfo('title', header.find('.//title', self.ns).text)
+		self._glos.setInfo('edition', header.find('.//edition', self.ns).text)
+		self._glos.setInfo('availability', self.to_string(header.find('.//availability', self.ns)))
+
+	def __init__(self, glos: GlossaryType):
+		self._glos = glos
+
+	def __len__(self) -> int:
+		return 0  # FIXME
+
+	def close(self) -> None:
+		pass
+
+	def open(self, filename: str):
+		self._filename = filename
+
+		context = ET.iterparse(filename, events=("end",))
+		for action, elem in context:
+			if elem.tag == f'{tei}teiHeader':
+				self.set_metadata(elem)
+				return
+
+	def __iter__(self) -> Iterator[BaseEntry]:
+		context = ET.iterparse(self._filename, events=("end",))
+		for action, elem in context:
+			if elem.tag == f'{tei}entry':
+				words, defi = self.get_entry_html(elem)
+				yield self._glos.newEntry(words, defi)
+				# clean up preceding siblings to save memory
+				while elem.getprevious() is not None:
+					del elem.getparent()[0]
+
 
 
 def write(
