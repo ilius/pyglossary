67c799fa2d6bbc6607affa150e99745a21efadd3
Saeed Rasooli <saeed.gnu@gmail.com>
2020-06-23 20:39:08 +0430

    plugins/testformat.py: switch to Reader class and write comments

--- pyglossary/plugins/testformat.py
+++ pyglossary/plugins/testformat.py
@@ -13,21 +13,53 @@ optionsProp = {}
 depends = {}
 
 
-def read(glos: GlossaryType, filename: str) -> bool:
-	log.info(f"reading from format {format} using plugin")
-	count = 100
-	# get number of entries from input file(depending on your format)
-	for i in range(count):
-		# here get word and definition from file(depending on your format)
-		word = f"word_{i}"
-		defi = f"definition {i}"
-		glos.addEntry(word, defi)
-	# here read info from file and set to Glossary object
-	glos.setInfo("name", "Test")
-	glos.setInfo("description", "Test glossary craeted by a PyGlossary plugin")
-	glos.setInfo("author", "Me")
-	glos.setInfo("copyright", "GPL")
-	return True  # reading input file was succesfull
+class Reader(object):
+	def __init__(self, glos: GlossaryType) -> None:
+		self._glos = glos
+		self._filename = ""
+		self._wordCount = 0
+
+	def __len__(self) -> int:
+		# return the number of entries if you have it
+		# if you don't, return 0 and progressbar will be disabled
+		# self._wordCount can be set in self.open function
+		# but if you want to set it, you should set it before
+		# iteration begins and __iter__ method is called
+		return self._wordCount
+
+	def open(self, filename) -> None:
+		# open the file, read headers / info and set info to self._glos
+		# and set self._wordCount if you can
+		# read-options should be keyword arguments in this method
+		self._wordCount = 100
+		log.info(f"reading from format {format} using plugin")
+		# here read info from file and set to Glossary object
+		self._glos.setInfo("name", "Test")
+		desc = "Test glossary craeted by a PyGlossary plugin"
+		self._glos.setInfo("description", desc)
+		self._glos.setInfo("author", "Me")
+		self._glos.setInfo("copyright", "GPL")
+
+	def close(self):
+		# this is called after reading/conversion is finished
+		# if you have an open file object, close it here
+		# if you need to clean up temp files, do it here
+		pass
+
+	def __iter__(self) -> "Iterator[BaseEntry]":
+		# the easiest and simplest way to implement an Iterator is
+		# by writing a generator, by calling: yield glos.newEntry(word, defi)
+		# inside a loop (typically iterating over a file object for text file)
+		# another way (which is harder) is by implementing __next__ method
+		# and returning self in __iter__
+		# that forces you to keep the state manually because __next__ is called
+		# repeatedly, but __iter__ is only called once
+		glos = self._glos
+		for i in range(self._wordCount):
+			# here get word and definition from file(depending on your format)
+			word = f"word_{i}"
+			defi = f"definition {i}"
+			yield glos.newEntry(word, defi)
 
 
 def write(glos: GlossaryType, filename: str) -> bool:
