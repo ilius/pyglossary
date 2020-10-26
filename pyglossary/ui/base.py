f692408ed5c1e553920c6a6c1cf3696e4f7cfeb6
Saeed Rasooli <saeed.gnu@gmail.com>
2020-10-26 23:54:22 +0330

    pep8 fixes in ui/base.py

--- pyglossary/ui/base.py
+++ pyglossary/ui/base.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-##
-## Copyright © 2012-2020 Saeed Rasooli <saeed.gnu@gmail.com> (ilius)
-## This file is part of PyGlossary project, https://github.com/ilius/pyglossary
-##
-## This program is a free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3, or (at your option)
-## any later version.
-##
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License along
-## with this program. Or on Debian systems, from /usr/share/common-licenses/GPL
-## If not, see <http://www.gnu.org/licenses/gpl.txt>.
+#
+# Copyright © 2012-2020 Saeed Rasooli <saeed.gnu@gmail.com> (ilius)
+# This file is part of PyGlossary project, https://github.com/ilius/pyglossary
+#
+# This program is a free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program. Or on Debian systems, from /usr/share/common-licenses/GPL
+# If not, see <http://www.gnu.org/licenses/gpl.txt>.
 
 from os.path import join, isfile
 import logging
@@ -36,9 +36,11 @@ from pyglossary.option import (
 	FloatOption,
 )
 
+
 def fread(path):
 	with open(path, encoding="utf-8") as fp:
 		return fp.read()
+
 
 log = logging.getLogger("pyglossary")
 logo = join(appResDir, "pyglossary.png")
@@ -114,6 +116,7 @@ class UIBase(object):
 		("reverse_maxNum", IntOption(cmd=False)),
 		("reverse_includeDefs", BoolOption(cmd=False)),
 	])
+
 	def loadConfig(self, **options):
 		data = jsonToData(fread(rootConfJsonFile))
 		if isfile(confJsonFile):
@@ -147,7 +150,3 @@ class UIBase(object):
 
 	def progressEnd(self):
 		self.progress(1.0)
-
-
-
-
