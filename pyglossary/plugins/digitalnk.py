fbbcd4f9a9f91dc1fb80133b68c063db58f3f5f5
Saeed Rasooli <saeed.gnu@gmail.com>
2021-01-08 15:58:14 +0330

    DigitalNK: work around Python's sqlite bug, #282

--- pyglossary/plugins/digitalnk.py
+++ pyglossary/plugins/digitalnk.py
@@ -46,7 +46,12 @@ class Reader(object):
 			"select word, definition from dictionary"
 			" order by word"
 		)
-		for row in self._cur:
+		# iteration over self._cur stops after one entry
+		# and self._cur.fetchone() returns None
+		# no idea why!
+		# https://github.com/ilius/pyglossary/issues/282
+		# for row in self._cur:
+		for row in self._cur.fetchall():
 			word = html.unescape(row[0])
 			definition = row[1]
 			yield self._glos.newEntry(word, definition, defiFormat="m")
