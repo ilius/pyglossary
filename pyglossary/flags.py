1cd543294fa585b42fd82a4b9f7d5f276e6255c8
Saeed Rasooli <saeed.gnu@gmail.com>
2021-10-01 16:06:01 +0330

    flags.py: change to sub-class of str and add .desc attribute

--- pyglossary/flags.py
+++ pyglossary/flags.py
@@ -1,7 +1,14 @@
-ALWAYS = 'always'
-DEFAULT_YES = 'default_yes'
-DEFAULT_NO = 'default_no'
-NEVER = 'never'
+class StrWithDesc(str):
+	def __new__(cls, name: str, desc: str):
+		s = str.__new__(cls, name)
+		s.desc = desc
+		return s
+
+
+ALWAYS = StrWithDesc("always", "Always")
+DEFAULT_YES = StrWithDesc("default_yes", "Yes (by default)")
+DEFAULT_NO = StrWithDesc("default_no", "No (by default)")
+NEVER = StrWithDesc("never", "Never")
 
 # typing.Literal is added in Python 3.8
 YesNoAlwaysNever = """Union[
