ed657b0c55e1125b3deb33d06c3e834e25222a47
Saeed Rasooli <saeed.gnu@gmail.com>
2021-11-25 02:44:39 +0330

    update doc/non-gui_examples/any_to_txt.py

--- doc/non-gui_examples/any_to_txt.py
+++ doc/non-gui_examples/any_to_txt.py
@@ -5,7 +5,16 @@ import pyglossary
 from pyglossary import Glossary
 from pyglossary.text_writer import writeTabfile
 
+# Glossary.init() must be called only once, so make sure you put it
+# in the right place
 Glossary.init()
 
 glos = Glossary()
-glos.convert(inputFilename=sys.argv[1], outputFilename=f"{sys.argv[1]}.txt")
+glos.convert(
+	inputFilename=sys.argv[1],
+	outputFilename=f"{sys.argv[1]}.txt",
+	# although it can detect format for *.txt, you can still pass outputFormat
+	outputFormat="Tabfile",
+	# you can pass readOptions or writeOptions as a dict
+	# writeOptions={"encoding": "utf-8"},
+)
