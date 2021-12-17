be45e649c04da78f995c6733f15d3870d797fdc4
Saeed Rasooli <saeed.gnu@gmail.com>
2021-12-17 14:20:56 +0330

    fix unclosed file in fileCountLines

--- pyglossary/file_utils.py
+++ pyglossary/file_utils.py
@@ -12,10 +12,10 @@ def toBytes(s: "AnyStr") -> bytes:
 
 def fileCountLines(filename: str, newline: str = "\n"):
 	newline = toBytes(newline)  # required? FIXME
-	f = open(filename, "rb")  # or "r"
-	bufgen = takewhile(
-		lambda x: x, (f.read(1024 * 1024) for _ in repeat(None))
-	)
-	return sum(
-		buf.count(newline) for buf in bufgen if buf
-	)
+	with open(filename, "rb") as _file:
+		bufgen = takewhile(
+			lambda x: x, (_file.read(1024 * 1024) for _ in repeat(None))
+		)
+		return sum(
+			buf.count(newline) for buf in bufgen if buf
+		)
