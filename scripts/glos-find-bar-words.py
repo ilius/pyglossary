6ff8d3ad3cd944cdffd13cb74970aba9e4c7de0d
Saeed Rasooli <saeed.gnu@gmail.com>
2021-12-19 18:28:55 +0330

    add scripts/glos-find-bar-words.py

---
+++ scripts/glos-find-bar-words.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+import sys
+from os.path import join, dirname, abspath
+
+rootDir = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, rootDir)
+
+from pyglossary import Glossary
+
+
+def hasBar(entry):
+	for word in entry.l_word:
+		if "|" in word:
+			return True
+	return False
+
+
+Glossary.init()
+
+for direct in (True, False):
+	print(f"\n-------- direct={direct}")
+
+	glos = Glossary()
+	glos.config = {
+		"enable_alts": True,
+	}
+	glos.read(
+		filename=sys.argv[1],
+		direct=direct,
+	)
+	for entry in glos:
+		if hasBar(entry):
+			print(f"+++ {entry.l_word!r} -> {entry.defi[:60]}")
+			continue
+
+		#print(f"--- {entry.l_word!r} -> {entry.defi[:60]}")
