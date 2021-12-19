d3b9c6a2936db5e2e92d7b24e712407e5b5119c0
Saeed Rasooli <saeed.gnu@gmail.com>
2021-12-19 11:28:17 +0330

    add tests for escapeNTB and splitByBarUnescapeNTB

--- pyglossary/text_utils_test.py
+++ pyglossary/text_utils_test.py
@@ -20,9 +20,38 @@ class TestTextUtils(unittest.TestCase):
 		self.assertEqual("a\\\tb", unescapeNTB("a\\\\\\tb", bar=False))
 		self.assertEqual("a|b\tc", unescapeNTB("a|b\\tc", bar=False))
 		self.assertEqual("a\\|b\tc", unescapeNTB("a\\|b\\tc", bar=False))
+		self.assertEqual("a\\|b\tc", unescapeNTB("a\\\\|b\\tc", bar=False))
 		self.assertEqual("|", unescapeNTB("\\|", bar=True))
 		self.assertEqual("a|b", unescapeNTB("a\\|b", bar=True))
 		self.assertEqual("a|b\tc", unescapeNTB("a\\|b\\tc", bar=True))
+
+	def test_escapeNTB(self):
+		self.assertEqual(escapeNTB("a", bar=False), "a")
+		self.assertEqual(escapeNTB("a\t", bar=False), "a\\t")
+		self.assertEqual(escapeNTB("a\n", bar=False), "a\\n")
+		self.assertEqual(escapeNTB("\ta", bar=False), "\\ta")
+		self.assertEqual(escapeNTB("\na", bar=False), "\\na")
+		self.assertEqual(escapeNTB("a\tb\n", bar=False), "a\\tb\\n")
+		self.assertEqual(escapeNTB("a\\b", bar=False), "a\\\\b")
+		self.assertEqual(escapeNTB("a\\\tb", bar=False), "a\\\\\\tb")
+		self.assertEqual(escapeNTB("a|b\tc", bar=False), "a|b\\tc")
+		self.assertEqual(escapeNTB("a\\|b\tc", bar=False), "a\\\\|b\\tc")
+		self.assertEqual(escapeNTB("|", bar=True), "\\|")
+		self.assertEqual(escapeNTB("a|b", bar=True), "a\\|b")
+		self.assertEqual(escapeNTB("a|b\tc", bar=True), "a\\|b\\tc")
+
+	def test_splitByBarUnescapeNTB(self):
+		f = splitByBarUnescapeNTB
+		self.assertEqual(f(""), [""])
+		self.assertEqual(f("|"), ["", ""])
+		self.assertEqual(f("a"), ["a"])
+		self.assertEqual(f("a|"), ["a", ""])
+		self.assertEqual(f("|a"), ["", "a"])
+		self.assertEqual(f("a|b"), ["a", "b"])
+		self.assertEqual(f("a\\|b|c"), ["a|b", "c"])
+		self.assertEqual(f("a\\\\1|b|c"), ["a\\1", "b", "c"])
+		# self.assertEqual(f("a\\\\|b|c"), ["a\\", "b", "c"])  # FIXME
+
 
 if __name__ == "__main__":
 	unittest.main()
