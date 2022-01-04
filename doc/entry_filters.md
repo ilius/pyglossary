9da459efd92af792f7bff65f8b491472030d6d92
Saeed Rasooli <saeed.gnu@gmail.com>
2022-01-04 05:52:06 +0330

    generate doc/entry_filters.md

---
+++ doc/entry_filters.md
@@ -0,0 +1,17 @@
+## Entry Filters
+
+| Name                         | Default Enabled | Command Flags                        | Description                                 |
+| ---------------------------- | --------------- | ------------------------------------ | ------------------------------------------- |
+| `strip`                      | Yes             |                                      | Strip whitespaces in word(s) and definition |
+| `non_empty_word`             | Yes             |                                      | Skip entries with empty word                |
+| `skip_resources`             | No              | `--skip-resources`                   | Skip resources / data files                 |
+| `fix_unicode`                | Yes             | `--utf8-check`<br/>`--no-utf8-check` | Fix Unicode in word(s) and definition       |
+| `lower_word`                 | No              | `--lower`<br/>`--no-lower`           | Lowercase word(s)                           |
+| `rtl`                        | No              | `--rtl`                              | Make definition right-to-left               |
+| `remove_html_all`            | No              | `--remove-html-all`                  | Remove all HTML tags from definition        |
+| `remove_html`                | No              | `--remove-html`                      | Remove specific HTML tags from definition   |
+| `normalize_html`             | No              | `--normalize-html`                   | Normalize HTML tags in definition (WIP)     |
+| `lang`                       | Yes             |                                      | Language-specific cleanup/fixes             |
+| `non_empty_word`             | Yes             |                                      | Skip entries with empty word                |
+| `non_empty_defi`             | Yes             |                                      | Skip entries with empty definition          |
+| `remove_empty_dup_alt_words` | Yes             |                                      | Remove empty and duplicate alternate words  |
---
+++ scripts/entry-filters-doc.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python3
+
+import sys
+import json
+from os.path import join, dirname, abspath
+from pprint import pprint
+from mako.template import Template
+
+rootDir = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, rootDir)
+
+from pyglossary.glossary import Glossary
+from pyglossary.core import userPluginsDir
+from pyglossary.ui.base import UIBase
+
+ui = UIBase()
+ui.loadConfig(user=False)
+
+template = Template("""${entryFiltersTable}
+""")
+
+
+def codeValue(x):
+	s = str(x)
+	if s:
+		return "`" + s + "`"
+	return ""
+
+
+def yesNo(x):
+	if x is True:
+		return "Yes"
+	if x is False:
+		return "No"
+	return ""
+
+
+def renderCell(value):
+	return str(value).replace("\n", "\\n").replace("\t", "\\t")
+
+
+def renderTable(rows):
+	"""
+		rows[0] must be headers
+	"""
+	rows = [
+		[
+			renderCell(cell) for cell in row
+		]
+		for row in rows
+	]
+	width = [
+		max(len(row[i]) for row in rows)
+		for i in range(len(rows[0]))
+	]
+	rows = [
+		[
+			cell.ljust(width[i], " ")
+			for i, cell in enumerate(row)
+		]
+		for rowI, row in enumerate(rows)
+	]
+	rows.insert(1, [
+		"-" * colWidth
+		for colWidth in width
+	])
+	return "\n".join([
+		"| " + " | ".join(row) + " |"
+		for row in rows
+	])
+
+
+def getCommandFlagsMD(configRule):
+	if configRule is None:
+		return ""
+	name = configRule[0]
+	opt = ui.configDefDict[name]
+	flag = name.replace("_", "-")
+
+	if opt.falseComment:
+		return f"`--{flag}`<br/>`--no-{flag}`"
+
+	return f"`--{flag}`"
+
+
+for configRule, filterClass in Glossary.entryFiltersRules:
+	if configRule is None:
+		continue
+	name, default = configRule
+	assert ui.config[name] == default
+
+
+entryFiltersTable = "## Entry Filters\n\n" + renderTable(
+	[(
+		"Name",
+		"Default Enabled",
+		"Command Flags",
+		"Description",
+	)] + [
+		(
+			codeValue(filterClass.name),
+			yesNo(configRule is None or bool(configRule[1])),
+			getCommandFlagsMD(configRule),
+			filterClass.desc,
+		)
+		for configRule, filterClass in Glossary.entryFiltersRules
+	],
+)
+
+text = template.render(
+	entryFiltersTable=entryFiltersTable,
+)
+with open(join(rootDir, "doc", "entry_filters.md"), mode="w") as _file:
+	_file.write(text)
