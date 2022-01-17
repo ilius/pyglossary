d00e8eec693f076485ce10961f4cc5f5a69fbd32
Saeed Rasooli <saeed.gnu@gmail.com>
2022-01-17 10:25:05 +0330

    add xml_utils_test.py

---
+++ tests/xml_utils_test.py
@@ -0,0 +1,25 @@
+import sys
+from os.path import dirname, abspath
+import unittest
+
+rootDir = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, rootDir)
+
+from pyglossary.xml_utils import xml_escape
+
+
+class Test_xml_escape(unittest.TestCase):
+	def test(self):
+		f = xml_escape
+		self.assertEqual(f(''), '')
+		self.assertEqual(f('abc'), 'abc')
+		self.assertEqual(f('"a"'), '&quot;a&quot;')
+		self.assertEqual(f("'a'"), "&apos;a&apos;")
+		self.assertEqual(f('"a"', quotation=False), '"a"')
+		self.assertEqual(f("'a'", quotation=False), "'a'")
+		self.assertEqual(f('R&D'), 'R&amp;D')
+		self.assertEqual(f('<-->'), '&lt;--&gt;')
+
+
+if __name__ == "__main__":
+	unittest.main()
