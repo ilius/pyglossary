d896375db64407bc775fab323bc5c795eba6343e
Saeed Rasooli <saeed.gnu@gmail.com>
2022-01-21 04:25:21 +0330

    update tests/g_babylon_bgl_test.py

--- tests/g_babylon_bgl_test.py
+++ tests/g_babylon_bgl_test.py
@@ -1,6 +1,7 @@
 import sys
 from os.path import dirname, abspath, join
 import unittest
+import hashlib
 
 rootDir = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, rootDir)
@@ -15,15 +16,26 @@ class TestGlossaryStarDict(TestGlossaryBase):
 
 		self.dataFileCRC32.update({
 			"Flavours_of_Malaysia.bgl": "46ef154b",
-			"Flavours_of_Malaysia.txt": "e62d738f",
 			"Flavours_of_Malaysia.txt_res/icon1.ico": "76a3b4c3",
+			"Currency_In_Each_Country.bgl": "309f1b3f",
+			"Solar_Physics_Glossary.bgl": "cc8f5ca1",
+			"Farsi_Aviation_Dictionary.bgl": "efa7bee4",
 		})
 
-	def convert_bgl_txt(self, fname, fname2, resFiles, **convertArgs):
+	def convert_bgl_txt(
+		self,
+		fname,
+		sha1sum=None,
+		md5sum=None,
+		resFiles=None,
+		**convertArgs
+	):
 		inputFilename = self.downloadFile(f"{fname}.bgl")
 		outputFname = f"{fname}-2.txt"
 		outputFilename = self.newTempFilePath(outputFname)
-		expectedFilename = self.downloadFile(f"{fname2}.txt")
+
+		if resFiles is None:
+			resFiles = {}
 		resFilesPath = {
 			resName: self.newTempFilePath(join(f"{outputFname}_res", resName))
 			for resName in resFiles
@@ -36,7 +48,17 @@ class TestGlossaryStarDict(TestGlossaryBase):
 			**convertArgs
 		)
 		self.assertEqual(outputFilename, res)
-		self.compareTextFiles(outputFilename, expectedFilename)
+
+		if sha1sum:
+			with open(outputFilename, mode="rb") as _file:
+				actualSha1 = hashlib.sha1(_file.read()).hexdigest()
+			self.assertEqual(actualSha1, sha1sum)
+
+		if md5sum:
+			with open(outputFilename, mode="rb") as _file:
+				actualMd5 = hashlib.md5(_file.read()).hexdigest()
+			self.assertEqual(actualMd5, md5sum)
+
 		for resName in resFiles:
 			resPathActual = resFilesPath[resName]
 			resPathExpected = self.downloadFile(f"{fname}.txt_res/{resName}")
@@ -45,8 +67,26 @@ class TestGlossaryStarDict(TestGlossaryBase):
 	def test_convert_bgl_txt_1(self):
 		self.convert_bgl_txt(
 			"Flavours_of_Malaysia",
-			"Flavours_of_Malaysia",
+			sha1sum="2b1fae135df2aaaeac23fb1dde497a4b6a22fd95",
 			resFiles=["icon1.ico"],
+		)
+
+	def test_convert_bgl_txt_2(self):
+		self.convert_bgl_txt(
+			"Currency_In_Each_Country",
+			sha1sum="731147c72092d813dfe1ab35d420477478832443",
+		)
+
+	def test_convert_bgl_txt_3(self):
+		self.convert_bgl_txt(
+			"Solar_Physics_Glossary",
+			sha1sum="f30b392c748c4c5bfa52bf7f9945c574617ff74a",
+		)
+
+	def test_convert_bgl_txt_4(self):
+		self.convert_bgl_txt(
+			"Farsi_Aviation_Dictionary",
+			sha1sum="b5741bf7ca4306f76ba414fe8efb637c59999d2b",
 		)
 
 
