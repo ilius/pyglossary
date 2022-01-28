d5670d8df20f3a5a2b02223b3072db1489897e9b
Saeed Rasooli <saeed.gnu@gmail.com>
2022-01-28 04:58:24 +0330

    iter_utils.py: remove unused key= argument from unique_everseen

--- pyglossary/iter_utils.py
+++ pyglossary/iter_utils.py
@@ -21,20 +21,12 @@
 
 
 # from https://github.com/erikrose/more-itertools
-def unique_everseen(iterable, key=None):
+def unique_everseen(iterable):
 	from itertools import filterfalse
 	"List unique elements, preserving order. Remember all elements ever seen."
 	# unique_everseen('AAAABBBCCDAABBB') --> A B C D
-	# unique_everseen('ABBCcAD', str.lower) --> A B C D
 	seen = set()
 	seen_add = seen.add
-	if key is None:
-		for element in filterfalse(seen.__contains__, iterable):
-			seen_add(element)
-			yield element
-	else:
-		for element in iterable:
-			k = key(element)
-			if k not in seen:
-				seen_add(k)
-				yield element
+	for element in filterfalse(seen.__contains__, iterable):
+		seen_add(element)
+		yield element
