96d6a82bb217967c51536b9f44f675b41a549e47
Saeed Rasooli <saeed.gnu@gmail.com>
2022-03-05 20:19:58 +0330

    add scripts/get-unlisted-formats.sh

---
+++ scripts/get-unlisted-formats.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+myPath=$(realpath "$0")
+myDir1=$(dirname "$myPath")
+rootDir=$(dirname "$myDir1")
+
+mkdir -p "$HOME/.cache/pyglossary/tmp"
+cd "$HOME/.cache/pyglossary/tmp"
+grep -oP 'doc/p/.*?\.md' "$rootDir/README.md" | sed 's|.*/||' | sort | uniq > formats-1
+
+grep -oP '/.*?\.md' "$rootDir/doc/p/__index__.md" | sed 's|.*/||' | sort > formats-2
+
+diff formats-1 formats-2 || true
+
+rm formats-1 formats-2
