8bfe2d14dd1d7376668efd7e6791214e391162ac
Saeed Rasooli <saeed.gnu@gmail.com>
2022-08-20 00:34:42 +0430

    WIP: xdxf

--- pyglossary/xdxf_transform.py
+++ pyglossary/xdxf_transform.py
@@ -9,6 +9,94 @@ from typing import Optional, Union
 
 log = logging.getLogger("pyglossary")
 
+# <k> and visual tags, no additional space/newline before/after them
+transparent_tags = {
+	"k",
+	"b", "i", "c",
+	"sub", "sup",
+}
+
+# Add a newline between following tags and any tag before/after (other than transparent_tags)
+block_tags = {
+	"co",  # editorial comment
+	"dtrn",  # Direct Translation of the key-phrase
+	"tr",  # transcription/pronunciation
+	"def",  # definition
+	"gr",  # grammer
+	"pos",  # not in standard, treat it like <gr>
+	"sr",  # semantic relations
+	"iref",  # child of <ex>
+	"ex_orig",  # child of <ex>
+	"ex_tran",  # child of <ex>
+}
+
+# No space/newline before/after <abr> (unless because of adjacent tags)
+
+# No space/newline before/after <kref>, unless inside <sr> (or because of adjacent tags)
+# So there must be newline between several <kref> inside <sr>
+
+# Add a newline between two directly adjacent "ex" tags, <ex>...</ex><ex>...</ex>
+
+# TODO: figure out rules for spaces
+
+
+
+def getTag(child: "Union[str, lxml.etree.Element]") -> str:
+	if isinstance(child, str):
+		return ""
+	return child.tag
+
+
+class IterationState(object):
+	def __init__(self):
+		self.elems = []
+		self.opaqueElems = []
+		self.lastElem = None
+		self.lastOpaqueElem = None
+
+	def open(self, elem) -> None:
+		tag = getTag(elem)
+		self.elems.append(elem)
+		if tag not in transparent_tags:
+			self.opaqueElems.append(elem)
+
+	def close(self):
+		elem = self.elems[-1]
+		self.elems = self.elems[:-1]
+		self.lastElem = elem
+		tag = getTag(elem)
+		if tag not in transparent_tags:
+			assert getTag(self.opaqueElems[-1]) == tag
+			self.opaqueElems = self.opaqueElems[:-1]
+			self.lastOpaqueElem = elem
+
+	@property
+	def tag(self):
+		if not self.elems:
+			return None
+		return getTag(self.elems[-1])
+
+	@property
+	def parentTag(self):
+		if len(self.elems) < 2:
+			return None
+		return getTag(self.elems[-2])
+
+	@property
+	def lastTag(self):
+		if self.lastElem is None:
+			return None
+		return getTag(self.lastElem)
+
+	@property
+	def lastOpaqueTag(self):
+		if self.lastOpaqueElem is None:
+			return None
+		return getTag(self.lastOpaqueElem)
+
+
+
+
 
 class XslXdxfTransformer(object):
 	_gram_color: str = "green"
@@ -114,15 +202,17 @@ class XdxfTransformer(object):
 			if hasPrev:
 				addSep()
 
-		child = child.rstrip()
+		#if self.shouldAddSep(child, prev):
+		#	addSep()
+
+		# child = child.rstrip()
 		lines = [line for line in child.split("\n") if line]
 		for index, line in enumerate(lines):
-			if index > 0:
-				# and line[0] not in ".,;)"
+			if self.shouldAddSep(line, None):
 				addSep()
 			hf.write(line)
-		if trail:
-			addSep()
+		#if trail:
+		#	addSep()
 		return
 
 	def writeExample(
@@ -138,8 +228,6 @@ class XdxfTransformer(object):
 		}):
 			for child in elem.xpath("child::node()"):
 				if isinstance(child, str):
-					# if not child.strip():
-					# 	continue
 					self.writeString(hf, child, elem, prev, stringSep=stringSep)
 					continue
 				if child.tag == "iref":
@@ -148,7 +236,7 @@ class XdxfTransformer(object):
 					continue
 				if child.tag in ("ex_orig", "ex_tran"):
 					with hf.element("div"):
-						self.writeChildrenOf(hf, child, stringSep=stringSep)  # NESTED 5
+						self.writeChildren(hf, child)  # NESTED 5
 					continue
 				# log.warning(f"unknown tag {child.tag} inside <ex>")
 				self.writeChild(hf, child, elem, prev, stringSep=stringSep)
@@ -173,7 +261,7 @@ class XdxfTransformer(object):
 				"class": "iref",
 				"href": child.attrib.get("href", child.text),
 			}):
-				self.writeChildrenOf(hf, child, stringSep=" ")
+				self.writeChildren(hf, child)
 
 	def writeChild(
 		self,
@@ -197,19 +285,19 @@ class XdxfTransformer(object):
 
 		if child.tag in ("i", "b", "sub", "sup", "tt", "big", "small"):
 			with hf.element(child.tag):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 				# if child.text is not None:
 				# 	hf.write(child.text.strip("\n"))
 			return
 
 		if child.tag == "blockquote":
 			with hf.element("div", **{"class": "m"}):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "tr":
 			hf.write("[")
-			self.writeChildrenOf(hf, child)
+			self.writeChildren(hf, child)
 			hf.write("]")
 			hf.write(ET.Element("br"))
 			return
@@ -219,12 +307,12 @@ class XdxfTransformer(object):
 				# with glos.titleElement(hf, child.text):
 				# ^ no glos object here!
 				with hf.element("b"):
-					self.writeChildrenOf(hf, child)
+					self.writeChildren(hf, child)
 			return
 
 		if child.tag == "sr":
 			with hf.element("div", **{"class": child.tag}):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "ex":
@@ -243,17 +331,17 @@ class XdxfTransformer(object):
 			with hf.element("span", **{"class": child.tag}):
 				with hf.element("font", color="green"):
 					with hf.element("i"):
-						self.writeChildrenOf(hf, child)  # NESTED 5
+						self.writeChildren(hf, child)  # NESTED 5
 			return
 
 		if child.tag in ("dtrn", "co"):
-			self.writeChildrenOf(hf, child, sep=" ")
+			self.writeChildren(hf, child)
 			return
 
 		if child.tag == "c":
 			color = child.attrib.get("c", "green")
 			with hf.element("font", color=color):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "kref":
@@ -280,16 +368,16 @@ class XdxfTransformer(object):
 			# TODO: create a list (ol / ul) unless it has one item only
 			# like FreeDict reader
 			with hf.element("div"):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "deftext":
-			self.writeChildrenOf(hf, child, stringSep=" ", sep=" ")
+			self.writeChildren(hf, child)
 			return
 
 		if child.tag == "span":
 			with hf.element("span"):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "abbr_def":
@@ -297,10 +385,10 @@ class XdxfTransformer(object):
 			# {"": "", "grm": "grammatical", "stl": "stylistical",
 			#  "knl": "area/field of knowledge", "aux": "subsidiary"
 			#  "oth": "others"}[_type]
-			self.writeChildrenOf(hf, child)
+			self.writeChildren(hf, child)
 			return
 
-		if child.tag == "gr":
+		if child.tag in ("gr", "pos"):
 			with hf.element("font", color=self._gram_color):
 				hf.write(child.text)
 			hf.write(ET.Element("br"))
@@ -308,18 +396,18 @@ class XdxfTransformer(object):
 
 		if child.tag == "ex_orig":
 			with hf.element("i"):
-				self.writeChildrenOf(hf, child)
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "ex_transl" and prev.tag == "ex_orig":
 			if child.text != prev.text:
 				with hf.element("i"):
-					self.writeChildrenOf(hf, child)
+					self.writeChildren(hf, child)
 			return
 
 		if child.tag == "categ":  # Category
 			with hf.element("span", style="background-color: green;"):
-				self.writeChildrenOf(hf, child, stringSep=" ")
+				self.writeChildren(hf, child)
 			return
 
 		if child.tag == "opt":
@@ -346,7 +434,7 @@ class XdxfTransformer(object):
 			return
 
 		log.warning(f"unknown tag {child.tag}")
-		self.writeChildrenOf(hf, child)
+		self.writeChildren(hf, child)
 
 	def shouldAddSep(
 		self,
@@ -368,19 +456,64 @@ class XdxfTransformer(object):
 
 		return True
 
-	def writeChildrenOf(
+	def shouldAddNewline(
+		self,
+		state: IterationState,
+	) -> bool:
+		if state.lastOpaqueElem is None:
+			return False
+
+		if state.lastOpaqueTag in block_tags:
+			return True
+
+		tag = state.tag
+
+		# This may add newline after opening a visual tag, for example:
+		# <gr>verb</gr><i><br/>something</i>
+		# instead of
+		# <gr>verb</gr><br/><i>something</i>
+		# but I don't have a clean solution for it now
+		if tag in transparent_tags:
+			return False
+
+		if tag in block_tags:
+			return True
+
+		if state.parentTag == "sr" and "kref" in (tag, state.lastOpaqueTag):
+			return True
+
+		# Add a newline between two directly adjacent "ex" tags, <ex>...</ex><ex>...</ex>
+		if tag == "ex" and state.lastTag == "ex":
+			return True
+
+		return False
+
+	def writeChildren(
 		self,
 		hf: "lxml.etree.htmlfile",
 		elem: "lxml.etree.Element",
-		sep: "Optional[str]" = None,
-		stringSep: "Optional[str]" = None,
 	):
-		prev = None
+		state = IterationState()
+		state.open(elem)
+		self.writeChildrenRec(hf, elem, state)
+		state.close()
+
+	def writeChildrenRec(
+		self,
+		hf: "lxml.etree.htmlfile",
+		elem: "lxml.etree.Element",
+		state: "IterationState",
+	):
+		from lxml import etree as ET
+
 		for index, child in enumerate(elem.xpath("child::node()")):
-			if sep and index > 0 and self.shouldAddSep(child, prev):
-				hf.write(sep)
-			self.writeChild(hf, child, elem, prev, stringSep=stringSep)
-			prev = child
+			state.open(child)
+			tag = getTag(child)
+			if self.shouldAddNewline(state):
+				hf.write(ET.Element("br"))
+			self.writeChild(hf, child, elem, state.lastElem)
+			state.close()
+
 
 	def transform(self, article: "lxml.etree.Element") -> str:
 		from lxml import etree as ET
@@ -388,7 +521,7 @@ class XdxfTransformer(object):
 		f = BytesIO()
 		with ET.htmlfile(f, encoding="utf-8") as hf:
 			with hf.element("div", **{"class": "article"}):
-				self.writeChildrenOf(hf, article)
+				self.writeChildren(hf, article)
 
 		text = f.getvalue().decode("utf-8")
 		text = text.replace("<br>", "<br/>")  # for compatibility
