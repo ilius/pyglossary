800449241045a3941c01b36a436e0466259cc1c4
Saeed Rasooli <saeed.gnu@gmail.com>
2023-02-16 03:41:04 +0330

    rename python file, xml file and class

--- tests/g_appledict_test.py
+++ tests/g_appledict_test.py
@@ -10,13 +10,13 @@ from tests.glossary_test import TestGlossaryBase
 from pyglossary.glossary import Glossary
 
 
-class TestGlossaryAppleDictSrc(TestGlossaryBase):
+class TestGlossaryAppleDict(TestGlossaryBase):
 	def __init__(self, *args, **kwargs):
 		TestGlossaryBase.__init__(self, *args, **kwargs)
 
 		self.dataFileCRC32.update({
 			"appledict-src/002-no-morphology.txt": "4afadab4",
-			"appledict-src/002-no-morphology-appledict-src-expected.xml": "60044894",
+			"appledict-src/002-no-morphology.xml": "60044894",
 		})
 
 	def test_tabfile_without_morpho_to_appledict_source(self):
@@ -25,7 +25,7 @@ class TestGlossaryAppleDictSrc(TestGlossaryBase):
 		baseName = "002-no-morphology"
 		inputFilepath = self.downloadFile(f"appledict-src/{baseName}.txt")
 
-		expectedOutputFilePath = self.downloadFile(f"appledict-src/{baseName}-appledict-src-expected.xml")
+		expectedOutputFilePath = self.downloadFile(f"appledict-src/{baseName}.xml")
 
 		outputDirPath = self.glos.convert(
 			inputFilename=inputFilepath,
