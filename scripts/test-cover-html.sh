3ab20c20301f53747c3b8790572c15d22b357295
Saeed Rasooli <saeed.gnu@gmail.com>
2023-02-17 19:38:35 +0330

    update scripts/test-cover-html.sh

--- scripts/test-cover-html.sh
+++ scripts/test-cover-html.sh
@@ -8,3 +8,5 @@ rootDir=$(dirname "$myDir1")
 cd "$rootDir/tests"
 coverage run -m unittest *_test.py
 coverage html --include="$rootDir/pyglossary/*" --omit="$rootDir/pyglossary/plugin_lib/*"
+
+echo "file://$rootDir/tests/htmlcov/index.html"
