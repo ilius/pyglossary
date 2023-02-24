5191dbf1fba7c5e4b5db2b71fc759f6cc1f9e0ef
Saeed Rasooli <saeed.gnu@gmail.com>
2023-02-24 05:01:43 +0330

    diff-glossary: compare info

--- pyglossary/ui/tools/diff_glossary.py
+++ pyglossary/ui/tools/diff_glossary.py
@@ -3,8 +3,10 @@
 import difflib
 import sys
 from subprocess import PIPE, Popen
+from typing import Tuple
 
 from pyglossary import Glossary
+from pyglossary.entry import Entry
 from pyglossary.glossary_type import EntryType
 from pyglossary.ui.tools.colors import (
 	green,
@@ -19,6 +21,23 @@ from pyglossary.ui.tools.word_diff import (
 )
 
 Glossary.init()
+
+
+def formatInfoValueDiff(diff) -> str:
+	a = ""
+	b = ""
+	for part in diff:
+		if part[0] == " ":
+			a += part[2:]
+			b += part[2:]
+			continue
+		if part[0] == "-":
+			a += red + part[2:] + reset
+			continue
+		if part[0] == "+":
+			b += green + part[2:] + reset
+			continue
+	return a + "\n" + b
 
 
 def diffGlossary(filename1, filename2, format1=None, format2=None):
@@ -42,6 +61,12 @@ def diffGlossary(filename1, filename2, format1=None, format2=None):
 	iter1 = iter(glos1)
 	iter2 = iter(glos2)
 
+	# infoIter1 = iter(sorted(glos1.iterInfo()))
+	# infoIter2 = iter(sorted(glos2.iterInfo()))
+
+	infoIter1 = glos1.iterInfo()
+	infoIter2 = glos2.iterInfo()
+
 	index1 = -1
 	index2 = -1
 
@@ -63,10 +88,23 @@ def diffGlossary(filename1, filename2, format1=None, format2=None):
 		)
 		proc.stdin.write(formatted.encode("utf-8"))
 
+	def printInfo(color: str, prefix: str, pair: "Tuple[str, str]"):
+		key, value = pair
+		spaces = " " * (len(prefix) + 7)
+		valueColor = color + spaces + value.replace("\n", "\n" + spaces + color)
+		formatted = (
+			f"{color}{prefix} Info: {key}\n"
+			f"{valueColor}"
+			"\n______________________________________________\n\n"
+		)
+		proc.stdin.write(formatted.encode("utf-8"))
+
 	def printChangedEntry(entry1, entry2):
 		defiDiff = formatDiff(xmlDiff(entry1.defi, entry2.defi))
 		entry1._defi = defiDiff
-		if index1 == index2:
+		if index1 < 0:
+			ids = ""
+		elif index1 == index2:
 			ids = f"#{index1}"
 		else:
 			ids = f"A#{index1} B#{index2}"
@@ -76,6 +114,43 @@ def diffGlossary(filename1, filename2, format1=None, format2=None):
 			"\n______________________________________________\n\n"
 		)
 		proc.stdin.write(formatted.encode("utf-8"))
+
+
+	def printChangedInfo(key, value1, value2):
+		valueDiff = formatInfoValueDiff(xmlDiff(value1, value2))
+		printInfo(yellow, "=== ", (key, valueDiff))
+
+
+	infoPair1 = None
+	infoPair2 = None
+
+	def newInfoEntry(pair: "Tuple[str, str]"):
+		key, value = pair
+		return Entry(f"Info: {key}", f"Value: {value}")
+
+	def infoStep():
+		nonlocal infoPair1, infoPair2
+		if infoPair1 is None:
+			infoPair1 = next(infoIter1)
+		if infoPair2 is None:
+			infoPair2 = next(infoIter2)
+
+		if infoPair1 == infoPair2:
+			infoPair1, infoPair2 = None, None
+			return
+
+		if infoPair1[0] == infoPair2[0]:
+			printChangedInfo(infoPair1[0], infoPair1[1], infoPair2[1])
+			infoPair1, infoPair2 = None, None
+			return
+
+		if infoPair1[0] < infoPair2[0]:
+			printInfo(red, "--- A: ", infoPair1)
+			infoPair1 = None
+			return
+
+		printInfo(green, "+++ B: ", infoPair2)
+		infoPair2 = None
 
 	def printAltsChangedEntry(entry1, entry2):
 		if index1 == index2:
@@ -102,7 +177,6 @@ def diffGlossary(filename1, filename2, format1=None, format2=None):
 			"\n______________________________________________\n\n"
 		)
 		proc.stdin.write(formatted.encode("utf-8"))
-
 
 	count = 0
 	entry1 = None
@@ -144,11 +218,37 @@ def diffGlossary(filename1, filename2, format1=None, format2=None):
 	try:
 		while True:
 			try:
+				infoStep()
+			except StopIteration:
+				break
+			except (BrokenPipeError, IOError):
+				break
+
+		if infoPair1:
+			printInfo(red, "--- A: ", infoPair1)
+		if infoPair2:
+			printInfo(green, "+++ B: ", infoPair2)
+
+		for pair in infoIter1:
+			printInfo(red, "--- A: ", pair)
+		for pair in infoIter2:
+			printInfo(green, "+++ B: ", pair)
+
+		while True:
+			try:
 				step()
 			except StopIteration:
 				break
 			except (BrokenPipeError, IOError):
 				break
+
+		if entry1:
+			printEntry(red, "--- A", index1, entry1)
+			index1 += 1
+		if entry2:
+			printEntry(green, "+++ B", index2, entry2)
+			index2 += 1
+
 		for entry in iter1:
 			printEntry(red, "--- A", index1, entry)
 			index1 += 1
