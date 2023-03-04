d07562f5883b3f5d3e6e31a3620e3f6129ddaacd
Saeed Rasooli <saeed.gnu@gmail.com>
2023-03-04 08:08:00 +0330

    add scripts/plist-to-json.py

---
+++ scripts/plist-to-json.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import biplist
+
+plistPath = sys.argv[1]
+
+try:
+    data = biplist.readPlist(plistPath)
+except (biplist.InvalidPlistException, biplist.NotBinaryPlistException):
+    try:
+        import plistlib
+        with open(plistPath, mode="rb") as plist_file:
+            data = plistlib.loads(plist_file.read())
+    except Exception as e:
+        raise IOError(
+            "'Info.plist' file is malformed, "
+            f"Please provide 'Contents/' with a correct 'Info.plist'. {e}",
+        ) from e
+
+print(json.dumps(data, indent="\t"))
