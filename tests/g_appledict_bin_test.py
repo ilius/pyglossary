74a1efdd5f8ec87818252416718871224c3b13de
Saeed Rasooli <saeed.gnu@gmail.com>
2023-05-02 12:10:48 +0330

    fix tests/g_appledict_bin_test.py, #442

--- tests/g_appledict_bin_test.py
+++ tests/g_appledict_bin_test.py
@@ -20,7 +20,7 @@ class TestGlossaryAppleDictBin(TestGlossaryBase):
 			"appledict-bin/002-simple.txt_res/style.css": "a83210cb",
 
 			"appledict-bin/006-en-oxfjord_v10.11_c2_t3.txt": "2d3844bf",
-			"appledict-bin/006-en-oxfjord_v10.11_c2_t3.txt_res/style.css": "d80d12f9",
+			"appledict-bin/006-en-oxfjord_v10.11_c2_t3.txt_res/style.css": "4aa2979f",
 		})
 
 		self.addDirCRC32("appledict-bin/002-simple.dictionary", {
