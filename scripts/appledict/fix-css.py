48eaf4769d1cf5aaea30ba1ae6af85f06c6ce622
Saeed Rasooli <saeed.gnu@gmail.com>
2023-05-03 22:06:14 +0330

    add scripts/appledict/fix-css.py

---
+++ scripts/appledict/fix-css.py
@@ -0,0 +1,19 @@
+import sys
+from os.path import dirname, realpath, splitext
+
+sys.path.insert(0, dirname(dirname(dirname(realpath(__file__)))))
+
+from pyglossary.apple_utils import substituteAppleCSS
+
+for fpath in sys.argv[1:]:
+	if fpath.endswith("-fixed.css"):
+		continue
+	fpathNoExt, _ = splitext(fpath)
+	fpathNew = fpathNoExt + "-fixed.css"
+	with open(fpath, "rt", encoding="utf-8") as _file:
+		text = _file.read()
+	text = substituteAppleCSS(text)
+	with open(fpathNew, "wt", encoding="utf-8") as _file:
+		_file.write(text)
+	print("Created", fpathNew)
+	print()
