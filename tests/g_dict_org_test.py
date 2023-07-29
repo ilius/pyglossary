5366e0a82b22efcd68c8f68c77b79d4cf6d86497
Saeed Rasooli <saeed.gnu@gmail.com>
2023-07-29 20:26:23 +0330

    add test for dict.org plugin

---
+++ tests/g_dict_org_test.py
@@ -0,0 +1,54 @@
+import sys
+import unittest
+from os.path import abspath, dirname
+
+rootDir = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, rootDir)
+
+from tests.glossary_v2_test import TestGlossaryBase
+
+
+class TestGlossaryDictOrg(TestGlossaryBase):
+	def __init__(self, *args, **kwargs):
+		TestGlossaryBase.__init__(self, *args, **kwargs)
+
+		self.dataFileCRC32.update({
+			"100-en-fa.txt.dict": "02abe5dc",
+			"100-en-fa.txt.index": "b10efcb4",
+			"100-en-fa.txt.index.txt": "6c9d527c",
+		})
+
+	def convert_txt_dict_org(self, fname, fname2, **convertArgs):
+		self.convert(
+			f"{fname}.txt",
+			f"{fname}-2.index",
+			compareText=f"{fname2}.index",
+			**convertArgs,
+		)
+
+	def convert_dict_org_txt(self, fname, fname2, **convertArgs):
+		self.downloadFile(f"{fname}.dict")
+		self.convert(
+			f"{fname}.index",
+			f"{fname}-2.txt",
+			compareText=f"{fname2}.txt",
+			**convertArgs,
+		)
+
+	def test_convert_txt_dict_org_1(self):
+		self.convert_txt_dict_org(
+			"100-en-fa",
+			"100-en-fa.txt",
+			writeOptions={"install": False},
+		)
+
+	def test_convert_dict_org_txt_1(self):
+		self.convert_dict_org_txt(
+			"100-en-fa.txt",
+			"100-en-fa.txt.index",
+			infoOverride={"input_file_size": None},
+		)
+
+
+if __name__ == "__main__":
+	unittest.main()
