049734348689b909b177d74e2195dba1034ad20a
Anton Karmanov <a.karmanov@inventati.org>
2023-09-30 18:56:50 +0300

    Skipt dictizip test cases on missing dependencies

--- tests/dictzip_test.py
+++ tests/dictzip_test.py
@@ -2,11 +2,13 @@ import gzip
 import tempfile
 from functools import partialmethod
 from pathlib import Path
+from typing import Callable
 
-import idzip as _  # noqa: F401
 from glossary_errors_test import TestGlossaryErrorsBase
 
 from pyglossary.os_utils import _dictzip, _idzip
+
+FUNC_TYPE = Callable[[str], bool]
 
 TEXT = """
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -18,24 +20,30 @@ culpa qui officia deserunt mollit anim id est laborum.
 """
 
 
-# TODO Check if dictzip in GH Action and avoid if not
-class DictzipTest(TestGlossaryErrorsBase):
-	def make_dz(self, func, path: Path) -> Path:
+class TestDictzipBase(TestGlossaryErrorsBase):
+	def _run(self, func: FUNC_TYPE, filename: str | Path) -> None:
+		done = func(str(filename))
+		if not done:
+			self.skipTest(f'Missing dependency for {func.__name__}')
+
+
+class TestDictzip(TestDictzipBase):
+	def make_dz(self, func: FUNC_TYPE, path: str | Path) -> Path:
 		"""Get path of dzipped file contains TEXT."""
 		test_file_path = Path(path)/"test_file.txt"
 		result_file_path = test_file_path.parent/(test_file_path.name + ".dz")
 		with open(test_file_path, "a") as tmp_file:
 			tmp_file.write(TEXT)
-		func(str(test_file_path))
+		self._run(func, test_file_path)
 		return result_file_path
 
-	def is_compressed_exists(self, func):
+	def is_compressed_exists(self, func: FUNC_TYPE) -> None:
 		with tempfile.TemporaryDirectory() as tmp_dir:
 			result_file_path = self.make_dz(func, tmp_dir)
 			self.assertTrue(result_file_path.exists())
 			self.assertTrue(result_file_path.is_file())
 
-	def is_compressed_matches(self, func):
+	def is_compressed_matches(self, func: FUNC_TYPE) -> None:
 		with tempfile.TemporaryDirectory() as tmp_dir:
 			result_file_path = self.make_dz(func, tmp_dir)
 			with gzip.open(result_file_path, 'r') as file:
@@ -49,14 +57,14 @@ class DictzipTest(TestGlossaryErrorsBase):
 	test_dictzip_compressed_matches = partialmethod(is_compressed_matches, _dictzip)
 
 
-class DictzipErrorsTest(TestGlossaryErrorsBase):
+class TestDictzipErrors(TestDictzipBase):
 	def tearDown(self):
 		self.mockLog.clear()
 		super().tearDown()
 
-	def on_missing_target(self, func):
+	def on_missing_target(self, func: FUNC_TYPE) -> None:
 		filename = '/NOT_EXISTED_PATH/file.txt'
-		func(filename)
+		self._run(func, filename)
 		err_num = self.mockLog.printRemainingErrors()
 		self.assertEqual(err_num, 1)
 
