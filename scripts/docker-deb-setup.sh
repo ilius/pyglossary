f8c433cbd40dc23c3f769aeec21207d402e37706
Saeed Rasooli <saeed.gnu@gmail.com>
2023-11-22 04:00:47 +0330

    Update docker-deb-setup.sh, #525

--- scripts/docker-deb-setup.sh
+++ scripts/docker-deb-setup.sh
@@ -13,6 +13,8 @@ apt-get install --yes python3-lzo
 apt-get install --yes python3-icu
 apt-get install --yes pkg-config
 
+rm /usr/lib/python*/EXTERNALLY-MANAGED
+
 pip3 install prompt_toolkit
 pip3 install beautifulsoup4
 pip3 install marisa-trie
