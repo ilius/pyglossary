8d6cc98ba15035d3fed747b9ea55bf130424837e
Saeed Rasooli <saeed.gnu@gmail.com>
2024-02-19 01:52:38 +0330

    update wcwidth to 0.2.13

--- pyglossary/ui/wcwidth/__init__.py
+++ pyglossary/ui/wcwidth/__init__.py
@@ -1,14 +1,4 @@
-'\nwcwidth module.\n\nhttps://github.com/jquast/wcwidth\n'
-from .wcwidth import (
-    WIDE_EASTASIAN,
-    ZERO_WIDTH,
-    _bisearch,
-    _wcmatch_version,
-    _wcversion_value,
-    list_versions,
-    wcswidth,
-    wcwidth,
-)
-
+'\nWcwidth module.\n\nhttps://github.com/jquast/wcwidth\n'
+from.wcwidth import ZERO_WIDTH,WIDE_EASTASIAN,VS16_NARROW_TO_WIDE,wcwidth,wcswidth,_bisearch,list_versions,_wcmatch_version,_wcversion_value
 __all__='wcwidth','wcswidth','list_versions'
-__version__='0.2.6'
+__version__='0.2.13'
