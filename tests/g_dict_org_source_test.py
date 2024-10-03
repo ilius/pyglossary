d01bbe7fe3a8e14bf6d38ee422a310f2f9c518c2
Saeed Rasooli <saeed.gnu@gmail.com>
2024-10-03 05:28:23 +0330

    tests: add g_dict_org_source_test.py

---
+++ tests/g_dict_org_source_test.py
@@ -0,0 +1,32 @@
+import unittest
+
+from glossary_v2_test import TestGlossaryBase
+
+
+class TestGlossaryDictOrg(TestGlossaryBase):
+	def __init__(self, *args, **kwargs):
+		TestGlossaryBase.__init__(self, *args, **kwargs)
+
+		self.dataFileCRC32.update(
+			{
+				"100-en-fa.dtxt": "05d6e939",
+			},
+		)
+
+	def convert_txt_dict_org_source(self, fname, fname2, **convertArgs):
+		self.convert(
+			f"{fname}.txt",
+			f"{fname}-2.dtxt",
+			compareText=f"{fname2}.dtxt",
+			**convertArgs,
+		)
+
+	def test_convert_txt_dict_org_source_1(self):
+		self.convert_txt_dict_org_source(
+			"100-en-fa",
+			"100-en-fa",
+		)
+
+
+if __name__ == "__main__":
+	unittest.main()
