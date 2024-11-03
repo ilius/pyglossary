6e7a778a421ad56c538c3fba8ac7d98b06be171d
Saeed Rasooli <saeed.gnu@gmail.com>
2024-11-03 07:19:25 +0330

    QuickDic6: add test

---
+++ tests/g_quickdic6_test.py
@@ -0,0 +1,51 @@
+import os
+import unittest
+
+from glossary_v2_test import TestGlossaryBase
+
+
+class TestGlossaryQuickDic6(TestGlossaryBase):
+	def __init__(self, *args, **kwargs):
+		TestGlossaryBase.__init__(self, *args, **kwargs)
+
+		self.dataFileCRC32.update(
+			{
+				"100-en-de-v4.txt": "d420a669",
+				"100-en-de-v4.txt.quickdic": "9d4ccc13",
+				"100-en-de-v4.txt.quickdic.txt": "2dc4fc17",
+			},
+		)
+
+		os.environ["QUICKDIC_CREATION_TIME"] = "2024-11-03T00:00:00.000000"
+
+	def convert_txt_quickdic(self, fname, sha1sum, **convertArgs):
+		self.convert(
+			f"{fname}.txt",
+			f"{fname}-2.quickdic",
+			sha1sum=sha1sum,
+			**convertArgs,
+		)
+
+	def convert_quickdic_txt(self, fname, fname2, **convertArgs):
+		self.convert(
+			f"{fname}.quickdic",
+			f"{fname}-2.txt",
+			compareText=f"{fname2}.txt",
+			**convertArgs,
+		)
+
+	def test_convert_txt_quickdic_1(self):
+		self.convert_txt_quickdic(
+			"100-en-de-v4",
+			"c8d9694624bace08e6e999db75c9156776f257c9",
+		)
+
+	def test_convert_quickdic_txt_1(self):
+		self.convert_quickdic_txt(
+			"100-en-de-v4.txt",
+			"100-en-de-v4.txt.quickdic",
+		)
+
+
+if __name__ == "__main__":
+	unittest.main()
