1443e05549aa87ff2c7d53f454a29c5fcc28beac
Saeed Rasooli <saeed.gnu@gmail.com>
2024-12-15 05:07:58 +0330

    update scripts/version-set.py

--- scripts/version-set.py
+++ scripts/version-set.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
+from datetime import datetime
 from os.path import abspath, dirname, join
 
 from packaging.version import parse
@@ -14,6 +16,15 @@ def main():
 	replaceVar(join(rootDir, "pyglossary/core.py"), "VERSION", versionQuoted)
 	replaceVar(join(rootDir, "setup.py"), "VERSION", versionQuoted)
 	replaceVar(join(rootDir, "pyproject.toml"), "version", versionQuoted)
+
+	# update copyright year number
+	for fname in ("about", "_license-dialog"):
+		with open(fname, encoding="utf-8") as file:
+			text = file.read()
+		pos = text.find("© ")
+		text = text[: pos + 7] + str(datetime.now().year) + text[pos + 11 :]
+		with open(fname, "w", encoding="utf-8") as file:
+			file.write(text)
 
 
 def replaceVar(fname: str, name: str, value: str) -> None:
