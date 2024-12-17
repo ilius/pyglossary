c848a7c644944c905dfb44f7c0b8be35a28b3975
Saeed Rasooli <saeed.gnu@gmail.com>
2024-12-17 15:39:46 +0330

    update scripts/test-cover-html*.sh

--- scripts/test-cover-html-plugin.sh
+++ scripts/test-cover-html-plugin.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# l_name of plugin, for example "stardict" or "octopus_mdict"
 pluginLname="$1"
+if [ -z "$pluginLname" ]; then
+	echo 'Must give plugins l_name as argument, for example "stardict" or "octopus_mdict"'
+	exit 1
+fi
 
-rootDir=$(dirname $(dirname "$0"))
+rootDir=$(dirname $(realpath $(dirname "$0")))
+echo $rootDir
 
 cd "$rootDir/tests"
 coverage run -m unittest "g_${pluginLname}_test.py"
-coverage html --include="$rootDir/pyglossary/plugins/${pluginLname}*"
+coverage html \
+	--include="$rootDir/pyglossary/plugins/${pluginLname}.py" \
+	--include="$rootDir/pyglossary/plugins/${pluginLname}/*.py" ||
+	echo "'coverage html' failed with $?"
+
+echo "file://$rootDir/tests/htmlcov/index.html"
--- scripts/test-cover-html.sh
+++ scripts/test-cover-html.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-rootDir=$(dirname $(dirname "$0"))
+rootDir=$(dirname $(realpath $(dirname "$0")))
+echo "file://$rootDir/tests/htmlcov/index.html"
 
 cd "$rootDir/tests"
 coverage run -m unittest ./*_test.py
-coverage html --include="$rootDir/pyglossary/*" --omit="$rootDir/pyglossary/plugin_lib/*"
+coverage html --include="$rootDir/pyglossary/*" --omit="$rootDir/pyglossary/plugin_lib/*" || echo "'coverage html' failed with $?"
 
 echo "file://$rootDir/tests/htmlcov/index.html"
