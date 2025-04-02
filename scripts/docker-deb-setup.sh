6e90c8e7e10ae9ae272cd955e856f526bb2fe2ff
Emfox Zhou <emfoxzhou@gmail.com>
2025-04-02 12:00:11 +0800

    docker: use Debian packages instead of pip (#644)

--- scripts/docker-deb-setup.sh
+++ scripts/docker-deb-setup.sh
@@ -12,11 +12,10 @@ apt-get install --yes python3-lxml
 apt-get install --yes python3-lzo
 apt-get install --yes python3-icu
 apt-get install --yes pkg-config
+apt-get install --yes python3-prompt-toolkit
+apt-get install --yes python3-bs4
+apt-get install --yes python3-marisa
+apt-get install --yes python3-libzim
+apt-get install --yes python3-mistune
 
 rm /usr/lib/python*/EXTERNALLY-MANAGED
-
-pip3 install prompt_toolkit
-pip3 install beautifulsoup4
-pip3 install marisa-trie
-pip3 install 'libzim>=1.0'
-pip3 install 'mistune==3.0.1'
