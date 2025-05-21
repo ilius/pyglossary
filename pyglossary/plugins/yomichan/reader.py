514bf7ecaf63696f51a3ab84a8c0f27dbaf2d105
Electro98 <geny_turov@mail.ru>
2025-05-21 20:47:14 +0900

    Yomichan Reader: add proper typing

--- pyglossary/plugins/yomichan/reader.py
+++ pyglossary/plugins/yomichan/reader.py
@@ -4,19 +4,21 @@ from __future__ import annotations
 import re
 from typing import (
     TYPE_CHECKING,
-    TypedDict,
 )
-from zipfile import ZipFile
+from zipfile import ZipFile, ZipInfo
 
 from pyglossary.json_utils import jsonToData
 
 if TYPE_CHECKING:
     from collections.abc import Generator, Iterator
-    from typing import Literal, TypeAlias
-
-    from typing_extensions import Required
 
     from pyglossary.glossary_types import EntryType, ReaderGlossaryType
+
+    from .types import (
+        DefinitionObj,
+        StructuredContent,
+        YomichanDefinition,
+    )
 
 __all__ = ["Reader"]
 
@@ -36,19 +38,24 @@ class Reader:
         self._dictFile: ZipFile | None = None
         self._filename = ""
         self._isSequenced = False
-        self._termFiles = []
+        self._termBankFiles = []
+        self._resourceFiles = []
 
     def open(self, filename: str) -> None:
         # TODO: Sanitize name
         self._filename = filename
         self._dictFile = ZipFile(filename)
-        termFiles: list[tuple[int, str]] = []
+        resourceFiles = []
+        termFiles: list[tuple[int, ZipInfo]] = []
         for file in self._dictFile.filelist:
             match = TERM_BASE_PATTERN.match(file.filename)
-            if match is None:
+            if match is not None:
+                termFiles.append((int(match.group(1)), file))
+            if file.filename == "index.json" or file.is_dir():
                 continue
-            termFiles.append((int(match.group(1)), file.filename))
-        self._termFiles = [val for _, val in sorted(termFiles, key=lambda v: v[0])]
+            resourceFiles.append(file)
+        self._termBankFiles = [val for _, val in sorted(termFiles, key=lambda v: v[0])]
+        self._resourceFiles = resourceFiles
         self._ReadIndex()
 
     def close(self) -> None:
@@ -57,22 +64,29 @@ class Reader:
         self.clear()
 
     def __len__(self) -> int:
-        # TODO: LEN
-        return 0
-
+        if self._dictFile is None:
+            raise RuntimeError("Yomichan: len(reader) called while reader is not open")
+        # TODO: how do I count real length??
+        return len(self._termBankFiles)
 
     def __iter__(self) -> Iterator[EntryType]:
-        for termFile in self._termFiles:
-            yield from self._ReadTermBank(termFile)
+        if self._dictFile is None:
+            raise RuntimeError("Yomichan: resources were read while reader is not open")
+        for termBankFile in self._termBankFiles:
+            yield from self._ReadTermBank(termBankFile.filename)
         yield from self._ReadUsedResources()
 
 
     def _ReadIndex(self) -> None:
+        if self._dictFile is None:
+            raise RuntimeError("Yomichan: resources were read while reader is not open")
         with self._dictFile.open("index.json") as indexFile:
             index = jsonToData(indexFile.read())
         assert isinstance(index, dict), "Invalid format"
         if index["format"] != 3:
-            raise NotImplementedError("Supported only dictionaries of 3 version")
+            raise NotImplementedError(
+                "Yomichan: Supported only dictionaries of 3 version",
+            )
         self._glos.setInfo("sourceLang", "ja")
         for c_field in FIELDS_TO_WRITE:
             value = index.get(c_field)
@@ -81,6 +95,8 @@ class Reader:
         self._isSequenced = index.get("isSequenced", False)
 
     def _ReadTermBank(self, termBankName: str) -> Generator[EntryType, None, None]:
+        if self._dictFile is None:
+            raise RuntimeError("Yomichan: resources were read while reader is not open")
         with self._dictFile.open(termBankName) as termBankFile:
             termBank = jsonToData(termBankFile.read())
         for term in termBank:
@@ -91,55 +107,15 @@ class Reader:
             yield self._glos.newEntry(word, definition, defiFormat="h")
 
     def _ReadUsedResources(self) -> Generator[EntryType, None, None]:
-        for file in self._dictFile.filelist:
-            if file.is_dir() or \
-                file.filename in self._termFiles or \
-                file.filename == "index.json":
-                continue
+        if self._dictFile is None:
+            raise RuntimeError("Yomichan: resources were read while reader is not open")
+        for file in self._resourceFiles:
             with self._dictFile.open(file.filename) as rawFile:
                 data = rawFile.read()
             yield self._glos.newDataEntry(file.filename, data)
 
 
-class StructuredContent(TypedDict, total=False):
-    type: Required[Literal["text", "structured-content", "img"]]
-    # Text
-    text: str
-    # Structured Content
-    content: StructuredContentChild
-    # Image
-    path: str
-    width: int
-    height: int
-    title: str
-    alt: str
-    description: str
-    pixelated: bool
-    imageRendering: Literal["auto", "pixelated", "crisp=edges"]
-    appearance: Literal["auto", "monochrome"]
-    background: bool
-    collapsed: bool
-    collapsible: bool
-
-
-class StructuredContentChildObj(TypedDict, total=False):
-    tag: Required[Literal["br", "div", "img", "a"]]
-    content: "StructuredContentChildObj"
-    data: dict[str, str]
-    # Local image file
-    path: str
-    # Only for links will be match for r"^(?:https?:|\?)[\w\W]*"
-    #   Internal links start from ?
-    href: str
-    lang: str
-
-
-if TYPE_CHECKING:
-    StructuredContentChild: TypeAlias = str | list["StructuredContentChild"] | "StructuredContentChildObj"
-    YomichanDefinitionType = str | StructuredContent | list
-
-
-def _ReadSubStructuredContent(elem: StructuredContentChild) -> str:
+def _ReadSubStructuredContent(elem: StructuredContent) -> str:
     if isinstance(elem, str):
         return elem
     if isinstance(elem, list):
@@ -162,19 +138,18 @@ def _ReadSubStructuredContent(elem: StructuredContentChild) -> str:
     return tag
 
 
-def _ReadStructuredContent(elem: StructuredContent) -> str:
-    elem_type = elem["type"]
-    if elem_type == "text":
+def _ReadStructuredContent(elem: DefinitionObj) -> str:
+    if elem["type"] == "text":
         return elem["text"]
-    if elem_type == "structured-content":
+    if elem["type"] == "structured-content":
         return _ReadSubStructuredContent(elem["content"])
-    if elem_type == "img":
-        return f"<img src=\"{elem['href']}\">"
+    if elem["type"] == "image":
+        return f"<img src=\"{elem['path']}\">"
     raise RuntimeError("Ill-formed Yomichan dictionary")
 
 
-def _ReadDefinition(definition: list[YomichanDefinitionType]) -> str:
-    def _ParseDefinition(defi: YomichanDefinitionType) -> str:
+def _ReadDefinition(definition: list[YomichanDefinition]) -> str:
+    def _ParseDefinition(defi: YomichanDefinition) -> str:
         if isinstance(defi, str):
             return defi
         if isinstance(defi, dict):
@@ -182,10 +157,3 @@ def _ReadDefinition(definition: list[YomichanDefinitionType]) -> str:
         raise NotImplementedError("Unknown elem in definition: {defi}")
 
     return "\n".join(map(_ParseDefinition, definition))
-
-
-# + Read zip file
-# - Read metadata from index.json
-# - Read definitions from term base
-# - transfer other data required by terms
-
