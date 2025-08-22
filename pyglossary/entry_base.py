b809e297414090fcaf74b43ab49e2b9c68023aee
Saeed Rasooli <saeed.gnu@gmail.com>
2025-08-22 07:38:02 +0330

    entry_base.py: use ABC and `@abstractmethod`

--- pyglossary/entry_base.py
+++ pyglossary/entry_base.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import typing
+from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -13,7 +14,7 @@ __all__ = ["BaseEntry", "MultiStr"]
 MultiStr: typing.TypeAlias = "str | list[str]"
 
 
-class BaseEntry:  # noqa: PLR0904
+class BaseEntry(ABC):  # noqa: PLR0904
 	__slots__: list[str] = [
 		"_term",
 	]
@@ -22,10 +23,91 @@ class BaseEntry:  # noqa: PLR0904
 		self._term: str | list[str]
 
 	@classmethod
-	def isData(cls) -> bool: ...
+	@abstractmethod
+	def isData(cls) -> bool:
+		raise NotImplementedError
 
+	@abstractmethod
 	def getFileName(self) -> str:
 		raise NotImplementedError
+
+	@property
+	@abstractmethod
+	def s_word(self) -> str:
+		raise NotImplementedError
+
+	@property
+	@abstractmethod
+	def l_word(self) -> list[str]:
+		raise NotImplementedError
+
+	@property
+	@abstractmethod
+	def lb_word(self) -> list[bytes]:
+		raise NotImplementedError
+
+	@property
+	@abstractmethod
+	def defi(self) -> str:
+		raise NotImplementedError
+
+	@property
+	@abstractmethod
+	def defiFormat(self) -> str:
+		# TODO: type: Literal["m", "h", "x", "b"]
+		raise NotImplementedError
+
+	@defiFormat.setter
+	@abstractmethod
+	def defiFormat(self, defiFormat: str) -> None:
+		# TODO: type: Literal["m", "h", "x", "b"]
+		raise NotImplementedError
+
+	@abstractmethod
+	def detectDefiFormat(self, default: str = "") -> str:
+		raise NotImplementedError
+
+	@abstractmethod
+	def addAlt(self, alt: str) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def editFuncWord(self, func: Callable[[str], str]) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def editFuncDefi(self, func: Callable[[str], str]) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def strip(self) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def replaceInWord(self, source: str, target: str) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def replaceInDefi(self, source: str, target: str) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def replace(self, source: str, target: str) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def byteProgress(self) -> tuple[int, int] | None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def removeEmptyAndDuplicateAltWords(self) -> None:
+		raise NotImplementedError
+
+	@abstractmethod
+	def stripFullHtml(self) -> str | None:
+		raise NotImplementedError
+
+	# ________________________________________________________
 
 	@property
 	def data(self) -> bytes:
@@ -38,19 +120,7 @@ class BaseEntry:  # noqa: PLR0904
 		raise NotImplementedError
 
 	@property
-	def s_word(self) -> str:
-		raise NotImplementedError
-
-	@property
-	def l_word(self) -> list[str]:
-		raise NotImplementedError
-
-	@property
-	def lb_word(self) -> list[bytes]:
-		raise NotImplementedError
-
-	@property
-	def defi(self) -> str:
+	def tmpPath(self) -> str | None:
 		raise NotImplementedError
 
 	@property
@@ -62,39 +132,3 @@ class BaseEntry:  # noqa: PLR0904
 	def b_defi(self) -> bytes:
 		"""Returns definition in bytes."""
 		return self.defi.encode("utf-8")
-
-	@property
-	def defiFormat(self) -> str:
-		# TODO: type: Literal["m", "h", "x", "b"]
-		...
-
-	@defiFormat.setter
-	def defiFormat(self, defiFormat: str) -> None:
-		# TODO: type: Literal["m", "h", "x", "b"]
-		...
-
-	def detectDefiFormat(self, default: str = "") -> str: ...
-
-	def addAlt(self, alt: str) -> None: ...
-
-	def editFuncWord(self, func: Callable[[str], str]) -> None: ...
-
-	def editFuncDefi(self, func: Callable[[str], str]) -> None: ...
-
-	def strip(self) -> None: ...
-
-	def replaceInWord(self, source: str, target: str) -> None: ...
-
-	def replaceInDefi(self, source: str, target: str) -> None: ...
-
-	def replace(self, source: str, target: str) -> None: ...
-
-	def byteProgress(self) -> tuple[int, int] | None: ...
-
-	def removeEmptyAndDuplicateAltWords(self) -> None: ...
-
-	def stripFullHtml(self) -> str | None: ...
-
-	@property
-	def tmpPath(self) -> str | None:
-		raise NotImplementedError
