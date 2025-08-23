15295a760e7f0ce0e497750f4c7f0ffde36bb5e5
glwnd <glwnd2030@gmail.com>
2025-08-23 10:42:19 +0200

    - cleanup debug

--- .github/scripts/mac/patch_sources.py
+++ .github/scripts/mac/patch_sources.py
@@ -16,18 +16,20 @@ Path("__init__.py").unlink(missing_ok=True)
 arg_main = Path('pyglossary/ui/argparse_main.py')
 arg_main.write_text(arg_main.read_text().replace('default="auto"', 'default="tk"'))
 
-plugin_path = Path(
-    ".venv") / "lib" / f"python{python_version}" / "site-packages" / "nuitka" / "plugins" / "standard" / "TkinterPlugin.py"
-
-if plugin_path.exists():
-    content = plugin_path.read_text(encoding="utf-8")
-    if '("8.5", "8.6")' in content:
-        plugin_path.write_text(
-            content.replace('("8.5", "8.6")', '("8.5", "8.6", "9.0")'),
-            encoding="utf-8"
-        )
-        print("TkinterPlugin.py updated for Tk 9.0 compatibility")
-    else:
-        print("TkinterPlugin.py already compatible or pattern not found")
-else:
-    print(f"{plugin_path} not found")
+# NO NEED: **already** patched upstream: https://github.com/Nuitka/Nuitka/blob/develop/nuitka/plugins/standard/TkinterPlugin.py#L81
+# NOT removing code in case of a future tk version bump
+# plugin_path = Path(
+#     ".venv") / "lib" / f"python{python_version}" / "site-packages" / "nuitka" / "plugins" / "standard" / "TkinterPlugin.py"
+#
+# if plugin_path.exists():
+#     content = plugin_path.read_text(encoding="utf-8")
+#     if '("8.5", "8.6")' in content:
+#         plugin_path.write_text(
+#             content.replace('("8.5", "8.6")', '("8.5", "8.6", "9.0")'),
+#             encoding="utf-8"
+#         )
+#         print("TkinterPlugin.py updated for Tk 9.0 compatibility")
+#     else:
+#         print("TkinterPlugin.py already compatible or pattern not found")
+# else:
+#     print(f"{plugin_path} not found")
