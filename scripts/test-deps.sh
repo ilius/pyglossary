54ab3b185fc3a77801096809bf605a35bad4b502
Saeed Rasooli <saeed.gnu@gmail.com>
2025-08-24 04:31:01 +0330

    add requirements-test.txt

--- scripts/test-deps.sh
+++ scripts/test-deps.sh
@@ -1,11 +1,1 @@
-python -m pip install \
-	freezegun \
-	PyICU \
-	lxml==5.3 \
-	beautifulsoup4 \
-	python-idzip \
-	polib \
-	html5lib \
-	mistune \
-	marisa-trie \
-	biplist
+python -m pip install -r requirements-test.txt
