2a3eaf85e759f00ef36fbba96d6b0e2e2f174c17
glowinthedark <48893368+glowinthedark@users.noreply.github.com>
2025-08-24 17:26:35 +0200

    Update make-dmg.sh

--- .github/scripts/mac/make-dmg.sh
+++ .github/scripts/mac/make-dmg.sh
@@ -6,4 +6,4 @@ VERSION="$4"
 VERSION_WITH_HASH="$5"
 # NOTE: using create-dmg might make smaller size DMGs
 # create-dmg --no-internet-enable --volname "${APPNAME}-${VERSION}" --volicon res/pyglossary.icns --eula LICENSE  --app-drop-link 50 50 "$APPNAME-${OS}-${VERSION_WITH_HASH}.dmg" "${DIST_DIR}/${APPNAME}.app"
-hdiutil create -verbose -volname "${APPNAME}-${VERSION}" -srcfolder "${DIST_DIR}/${APPNAME}.app" -ov -format UDZO -fs HFS+J "${APPNAME}-{$OS}-${VERSION_WITH_HASH}.dmg"
+hdiutil create -verbose -volname "$APPNAME-$VERSION_WITH_HASH" -srcfolder "$DIST_DIR/$APPNAME.app" -ov -format UDZO -fs HFS+J "$APPNAME-$OS-$VERSION.dmg"
