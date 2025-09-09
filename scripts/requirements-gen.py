4f79162eb90f303c05b21ab8f673d677ff8f2f62
Saeed Rasooli <saeed.gnu@gmail.com>
2025-09-09 09:14:47 +0330

    scripts/requirements-gen.py: ensure module names are added to exclude_toplevel_module

--- scripts/requirements-gen.py
+++ scripts/requirements-gen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import tomllib as toml
 from importlib.metadata import distribution
 from os.path import abspath, dirname
 from pathlib import Path
@@ -21,19 +22,35 @@ plugins = [
 	p for p in Glossary.plugins.values() if userPluginsDirPath not in p.path.parents
 ]
 
-depSet = {
+requirements = {
+	"prompt_toolkit",  # used for interactive cli
+}
+moduleNames = {
 	"prompt_toolkit",  # used for interactive cli
 }
 # "tqdm" used for progressbar if installed
 # "python-idzip" is used as dictzip for StarDict if installed
 for p in plugins:
-	depSet |= set(p.readDepends.values())
-	depSet |= set(p.writeDepends.values())
+	requirements |= set(p.readDepends.values())
+	requirements |= set(p.writeDepends.values())
+	moduleNames |= set(p.readDepends)
+	moduleNames |= set(p.writeDepends)
 
-for dep in depSet:
-	name = dep.strip().split(">")[0]
+for reqFull in requirements:
+	name = reqFull.strip().split(">")[0]
 	distName = distribution(name).name
 	assert name == distName, f"{name=}, {distName=}"
 
 with open("requirements.txt", "w", encoding="utf-8") as file:
-	file.writelines(name + "\n" for name in sorted(depSet))
+	file.writelines(name + "\n" for name in sorted(requirements))
+
+
+with open("pyproject.toml", mode="rb") as file:
+	project = toml.load(file)
+
+import_analyzer = project["tool"]["import-analyzer"]
+exclude_toplevel_module: list[str] = import_analyzer["exclude_toplevel_module"]
+# print(exclude_toplevel_module)
+
+for modName in moduleNames:
+	assert modName in exclude_toplevel_module, modName
