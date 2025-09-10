f89f65502ebe0bdd76fd8f074da7d84ab6eae191
Saeed Rasooli <saeed.gnu@gmail.com>
2025-09-10 22:27:30 +0330

    github: use ruff 0.13.0

--- .github/scripts/get-ruff.sh
+++ .github/scripts/get-ruff.sh
@@ -1,4 +1,4 @@
-wget -c https://github.com/astral-sh/ruff/releases/download/0.12.4/ruff-x86_64-unknown-linux-gnu.tar.gz
+wget -c https://github.com/astral-sh/ruff/releases/download/0.13.0/ruff-x86_64-unknown-linux-gnu.tar.gz
 tar -xzf ruff-*.tar.gz
 mv ruff-x86_64-unknown-linux-gnu/ruff .
 ls -l ruff
