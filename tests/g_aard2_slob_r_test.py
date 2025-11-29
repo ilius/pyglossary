c003874901e31c679843127c3bb5e0429b496353
Saeed Rasooli <saeed.gnu@gmail.com>
2025-11-29 12:22:26 +0330

    break up slob test into 2 files for read and write

--- tests/g_aard2_slob_r_test.py
+++ tests/g_aard2_slob_r_test.py
@@ -4,7 +4,7 @@ import unittest
 from glossary_v2_test import TestGlossaryBase
 
 
-class TestGlossarySlob(TestGlossaryBase):
+class TestGlossarySlobRead(TestGlossaryBase):
 	def __init__(self, *args, **kwargs):
 		TestGlossaryBase.__init__(self, *args, **kwargs)
 
@@ -13,56 +13,15 @@ class TestGlossarySlob(TestGlossaryBase):
 				"100-en-fa-res.slob": "0216d006",
 				"100-en-fa-res-slob.txt": "c73100b3",
 				"100-en-fa-res-slob-sort.txt": "8253fe96",
-				"300-ru-en.txt": "77cfee2f",
 			},
 		)
 
 	def setUp(self):
-		if os.getenv("SKIP_SLOB_TEST"):
-			self.skipTest("skipping aard2 slob test because of env var SKIP_SLOB_TEST")
-		TestGlossaryBase.setUp(self)
-
-	def test_convert_txt_slob_1(self):
-		fname = "100-en-fa"
-		os.environ["SLOB_TIMESTAMP"] = "2023-01-01T12:00:00.000000+00:00"
-		self.convert(
-			f"{fname}.txt",
-			f"{fname}.slob",
-			# sha1sum="",
-			# compareBinary="",
-			# slob file is different each time (and so its sha1sum and md5sum)
-			# even with same exact tags!
-			# writeOptions={"compression": ""},
-		)
-
-	def test_convert_txt_slob_2_file_size_approx(self):
-		fname = "300-ru-en"
-		file_size_approx = 25000
-		files = [
-			(35852, self.newTempFilePath("300-ru-en.slob")),
-			(35687, self.newTempFilePath("300-ru-en.1.slob")),
-			(33856, self.newTempFilePath("300-ru-en.2.slob")),
-			(29413, self.newTempFilePath("300-ru-en.3.slob")),
-		]
-		self.convert(
-			f"{fname}.txt",
-			f"{fname}.slob",
-			writeOptions={
-				"file_size_approx": file_size_approx,
-				"file_size_approx_check_num_entries": 1,
-			},
-			compareBinary="",
-			# slob file is different each time (and so its sha1sum and md5sum)
-		)
-		for size, fpath in files:
-			with open(fpath, mode="rb") as file:
-				actualSize = len(file.read())
-			delta = actualSize - size
-			self.assertLess(
-				delta,
-				100,
-				msg=f"size expected={size} actual={actualSize}, file {fpath}",
+		if os.getenv("SKIP_SLOB_READ_TEST"):
+			self.skipTest(
+				"skipping aard2 slob read test because of env var SKIP_SLOB_READ_TEST"
 			)
+		TestGlossaryBase.setUp(self)
 
 	def convert_slob_txt(self, fname, fname2, resFiles, **convertArgs):
 		resFilesPath = {
---
+++ tests/g_aard2_slob_w_test.py
@@ -0,0 +1,68 @@
+import os
+import unittest
+
+from glossary_v2_test import TestGlossaryBase
+
+
+class TestGlossarySlobWrite(TestGlossaryBase):
+	def __init__(self, *args, **kwargs):
+		TestGlossaryBase.__init__(self, *args, **kwargs)
+
+		self.dataFileCRC32.update(
+			{
+				"300-ru-en.txt": "77cfee2f",
+			},
+		)
+
+	def setUp(self):
+		if os.getenv("SKIP_SLOB_WRITE_TEST"):
+			self.skipTest(
+				"skipping aard2 slob write test because of env var SKIP_SLOB_WRITE_TEST"
+			)
+		TestGlossaryBase.setUp(self)
+
+	def test_convert_txt_slob_1(self):
+		fname = "100-en-fa"
+		os.environ["SLOB_TIMESTAMP"] = "2023-01-01T12:00:00.000000+00:00"
+		self.convert(
+			f"{fname}.txt",
+			f"{fname}.slob",
+			# sha1sum="",
+			# compareBinary="",
+			# slob file is different each time (and so its sha1sum and md5sum)
+			# even with same exact tags!
+			# writeOptions={"compression": ""},
+		)
+
+	def test_convert_txt_slob_2_file_size_approx(self):
+		fname = "300-ru-en"
+		file_size_approx = 25000
+		files = [
+			(35852, self.newTempFilePath("300-ru-en.slob")),
+			(35687, self.newTempFilePath("300-ru-en.1.slob")),
+			(33856, self.newTempFilePath("300-ru-en.2.slob")),
+			(29413, self.newTempFilePath("300-ru-en.3.slob")),
+		]
+		self.convert(
+			f"{fname}.txt",
+			f"{fname}.slob",
+			writeOptions={
+				"file_size_approx": file_size_approx,
+				"file_size_approx_check_num_entries": 1,
+			},
+			compareBinary="",
+			# slob file is different each time (and so its sha1sum and md5sum)
+		)
+		for size, fpath in files:
+			with open(fpath, mode="rb") as file:
+				actualSize = len(file.read())
+			delta = actualSize - size
+			self.assertLess(
+				delta,
+				100,
+				msg=f"size expected={size} actual={actualSize}, file {fpath}",
+			)
+
+
+if __name__ == "__main__":
+	unittest.main()
