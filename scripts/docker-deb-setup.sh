e5a175a64aa9842c0348d1f63056b00c66c6d827
Saeed Rasooli <saeed.gnu@gmail.com>
2025-12-24 05:15:33 +0330

    use shebang `#!/usr/bin/env bash`

--- scripts/docker-deb-setup.sh
+++ scripts/docker-deb-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 rm /etc/apt/apt.conf.d/docker-clean
 
--- scripts/type-checker-deps.sh
+++ scripts/type-checker-deps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 python3 -m pip install lxml-stubs types-beautifulsoup4 types-psutil types-polib
 
