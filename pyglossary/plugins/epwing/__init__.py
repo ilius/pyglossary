147dcb84282ef987e2b9a04f5c2d9a1fea6baf16
Saeed Rasooli <saeedgnu@riseup.net>
2026-03-20 16:16:18 +0800

    Add wiki and website links for EPWING plugin

--- pyglossary/plugins/epwing/__init__.py
+++ pyglossary/plugins/epwing/__init__.py
@@ -28,8 +28,11 @@ description = "EPWING"
 extensions = ()
 singleFile = False
 kind = "directory"
-wiki = ""
 sortOnWrite = ALWAYS
 sortKeyName = "headword"
-
+wiki = "https://en.wikipedia.org/wiki/EPWING"
+website = (
+	"https://web.archive.org/web/20060430114516/http://www.epwing.or.jp/",
+	"EPWING Consortium (2006)",
+)
 optionsProp: dict[str, Option] = {}
