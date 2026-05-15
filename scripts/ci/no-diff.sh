29a3e123a716504244834070fa4f29d66af0637e
Saeed Rasooli <saeed.gnu@gmail.com>
2026-05-16 05:17:31 +0800

    update scripts/ci/no-diff.sh

--- scripts/ci/no-diff.sh
+++ scripts/ci/no-diff.sh
@@ -2,6 +2,6 @@ CHANGES=$(GIT_PAGER= git diff --name-only HEAD --)
 if [ -n "$CHANGES" ]; then
 	echo "There are changes after running gen:"
 	echo "$CHANGES"
-	git diff
+	GIT_PAGER= git diff
 	exit 1
 fi
